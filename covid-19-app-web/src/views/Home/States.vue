6c98761e8340cbde9f43f7bf2a08a49be8dd52da
yiseboge <Ethiopia1!>
2020-04-20 16:09:52 +0300

    [frontend] Implement Skeleton web pages

---
+++ covid-19-app-web/src/views/Home/States.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="states">
+    <!--    TODO Replace code here-->
+    <h1 class="text-center font-weight-thin">
+      States Demonstration Goes here
+    </h1>
+  </section>
+</template>
+
+<script>
+</script>
---
+++ covid-19-app-web/src/views/News/News.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-container class="news">
+    <h1 class="text-center font-weight-thin">Here goes the news</h1>
+    <!--    TODO Replace code here-->
+  </v-container>
+</template>
