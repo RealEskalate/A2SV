b74ec129c9bde8457b7ffcd67c850739d2d9f686
TripleThreads <programmers2017@outlook.com>
2020-04-21 16:39:20 +0300

    news starter designs

--- covid-19-app-web/src/views/News/News.vue
+++ covid-19-app-web/src/views/News/News.vue
@@ -1,6 +1,33 @@
 <template>
-  <v-container class="news">
-    <h1 class="text-center font-weight-thin">Here goes the news</h1>
-    <!--    TODO Replace code here-->
-  </v-container>
+    <v-container class="news">
+        <v-row>
+            <v-col cols="8">
+                <featured-news/>
+                <news-list/>
+            </v-col>
+            <v-col cols="4">
+                <top-news/>
+            </v-col>
+        </v-row>
+        <!--    TODO Replace code here-->
+    </v-container>
 </template>
+<script>
+    import TopNews from "./TopNews";
+    import NewsList from "./NewsList";
+    import FeaturedNews from "./FeaturedNews";
+
+    export default {
+        components: {
+            TopNews,
+            NewsList,
+            FeaturedNews
+        }
+    }
+</script>
+
+<style>
+    .news {
+        font-family: "Roboto", sans-serif;
+    }
+</style>
