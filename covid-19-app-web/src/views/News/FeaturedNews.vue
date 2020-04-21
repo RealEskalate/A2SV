a4e7d929fad6b8db965e0f7034b1400e3759240b
TripleThreads <programmers2017@outlook.com>
2020-04-21 16:39:20 +0300

    news starter designs

---
+++ covid-19-app-web/src/views/News/FeaturedNews.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <v-card class="mx-auto row shadow-lg overflow-hidden">
+            <v-row>
+                <v-container class="col-md-7">
+                    <div class="overline mb-4">Featured</div>
+                    <h1 class="font-weight-light">
+                        Pakistan records highest number of coronavirus deaths in a day
+                    </h1>
+                </v-container>
+                <v-img class="col-md-4"
+                       src="https://www.aljazeera.com/mritems/imagecache/mbdxlarge/mritems/Images/2020/4/21/0e8b725e55c2414cb7404885985ed318_18.jpg">
+                </v-img>
+            </v-row>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "FeaturedNews"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ covid-19-app-web/src/views/News/TopNews.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-card raised elevation="6" shaped>
+        <v-card-title>
+            <h2 class="text-center font-weight-medium mx-auto">Top News</h2>
+        </v-card-title>
+        <v-list dense>
+            <v-list-item-group v-model="item" color="primary">
+                <v-list-item
+                        v-for="(item, i) in items"
+                        :key="i"
+                >
+                    <v-list-item-content>
+                        <v-list-item-title class="text--accent-1 font-weight-medium" v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+                    <span>
+                        <v-list-item-icon class="font-weight-light">
+                            <small class="grey--text text--lighten-1">{{ item.numberOfViews }}</small>
+                            <v-icon class="ml-2 grey--text text--lighten-1" >mdi-eye</v-icon>
+                        </v-list-item-icon>
+                    </span>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "TopNews",
+        data: () => ({
+            items: [
+                {text: 'Trump to suspend all immigration into US over coronavirus.', icon: 'mdi-eye', numberOfViews: 120},
+                {text: 'Three more cases in Ethiopia', icon: 'mdi-account', numberOfViews: 112},
+                {text: 'A2SV team is combating corona', icon: 'mdi-flag',numberOfViews: 100},
+            ],
+        }),
+    }
+</script>
+
+<style scoped>
+
+</style>
