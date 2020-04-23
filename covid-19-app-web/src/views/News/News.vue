e3c5a7fbe957e378b4bf57b0ce5b21582b42b5c5
eyosias5 <gemmechumohammed@gmail.com>
2020-04-23 10:49:32 +0300

    [web] Connect with api

--- covid-19-app-web/src/views/News/News.vue
+++ covid-19-app-web/src/views/News/News.vue
@@ -4,7 +4,6 @@
       <v-row align="center">
         <v-col cols="6">
           <v-select
-            v-model="selected"
             :items="items"
             :menu-props="{ top: false, offsetY: true }"
             label="Scope"
@@ -14,77 +13,37 @@
     </section>
     <v-row no-gutters>
       <v-col :cols="8">
-        <div v-if="selected == 'Global'">
-          <div v-for="newss in visiblePages" :key="newss._id">
-            <v-card max-width="500" class="">
-              <v-list-item>
-                <v-list-item-content>
-                  <v-list-item-title class="headline text-wrap">{{
-                    newss.title
-                  }}</v-list-item-title>
-                  <v-list-item-subtitle
-                    >by {{ newss.source }}
-                    <v-spacer></v-spacer>
-                    {{ getTime(newss.date) }}
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-
-              <v-card-text v-html="newss.description" />
-              <v-card-actions>
-                <v-btn
-                  text
-                  color="deep-purple accent-4"
-                  v-bind:href="newss.reference_link"
-                  target="blank"
+        <div v-for="newss in news" :key="newss._id">
+          <v-card max-width="500" class="">
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="headline text-wrap">{{
+                  newss.title
+                }}</v-list-item-title>
+                <v-list-item-subtitle
+                  >by {{ newss.source }}</v-list-item-subtitle
                 >
-                  Read
-                </v-btn>
-               </v-card-actions>
-            </v-card>
-            <br />
-          </div>
-          <v-pagination
-            class="v-paggination"
-            v-model="page"
-            :length="Math.ceil(news.length / perPage)"
-          ></v-pagination>
-        </div>
-        <div v-else>
-          <div v-for="newss in visiblePages.slice(0, 3)" :key="newss._id">
-            <v-card max-width="500" class="">
-              <v-list-item>
-                <v-list-item-content>
-                  <v-list-item-title class="headline text-wrap">{{
-                    newss.title
-                  }}</v-list-item-title>
-                  <v-list-item-subtitle
-                    >by {{ newss.source }}
-                    <v-spacer></v-spacer>
-                    {{ getTime(newss.date) }}
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
+              </v-list-item-content>
+            </v-list-item>
 
-              <v-card-text v-html="newss.description" />
-              <v-card-actions>
-                <v-btn
-                  text
-                  color="deep-purple accent-4"
-                  v-bind:href="newss.reference_link"
-                  target="blank"
-                >
-                  Read
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-            <br />
-          </div>
-          <v-pagination
-            class="v-paggination"
-            v-model="page"
-            :length="Math.ceil(news.length / perPage)"
-          ></v-pagination>
+            <v-card-text v-html="newss.description"/>
+            <v-card-actions>
+              <v-btn
+                text
+                color="deep-purple accent-4"
+                v-bind:href="newss.reference_link"
+                target="blank"
+              >
+                Read
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-share-variant</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <br />
+          <br />
         </div>
       </v-col>
 
@@ -95,18 +54,9 @@
             <v-tab>Most view</v-tab>
             <v-tab-item>
               <v-card flat tile>
-                <div
-                  v-for="newsss in visiblePages.slice(0, 6)"
-                  :key="newsss._id"
-                >
-                  <v-list-item
-                    v-bind:href="newsss.reference_link"
-                    target="blank"
-                  >
-                    <v-card-text>{{ newsss.title }}</v-card-text>
-                  </v-list-item>
-
-                  <hr class="recent-v-card-text" />
+                <div v-for="newsss in news" :key="newsss._id">
+                <v-card-text>{{ newsss.title }}</v-card-text>
+                <hr class="recent-v-card-text">
                 </div>
               </v-card>
             </v-tab-item>
@@ -124,12 +74,8 @@
 
 <script>
 import axios from "axios";
-import moment from "moment";
 export default {
   data: () => ({
-    page: 1,
-    perPage: 10,
-    selected: "Global",
     items: ["Global", "Local"],
     news: []
   }),
@@ -144,20 +90,6 @@ export default {
         this.errored = true;
       })
       .finally(() => (this.loading = false));
-  },
-  methods: {
-    getTime(postDate) {
-      postDate = moment(String(postDate)).format("MM/DD/YYYY hh:mm");
-      return postDate;
-    }
-  },
-  computed: {
-    visiblePages() {
-      return this.news.slice(
-        (this.page - 1) * this.perPage,
-        this.page * this.perPage
-      );
-    }
   }
 };
 </script>
@@ -165,13 +97,8 @@ export default {
 <style scoped>
 .wrap-text {
   -webkit-line-clamp: unset !important;
-  font-size: 1.15rem !important;
-  line-height: 1.5rem;
 }
-.recent-v-card-text {
+.recent-v-card-text{
   color: darkgray;
-}
-.v-paggination {
-  justify-content: start;
 }
 </style>
