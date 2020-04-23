bd3cc380e30ff85bfda9ad6f9e3d1cc96323ab4c
Khalid Sultan <kalid.sultan.95@gmail.com>
2020-04-23 10:10:57 +0300

    [db] Fixed bug

--- Hack/Backend/routes/LogRoutes.js
+++ Hack/Backend/routes/LogRoutes.js
@@ -7,7 +7,7 @@ var log_controller = require("../controllers/LogController");
 
 router.get("/api/logs", log_controller.get_logs);
 router.get("/api/logs/:id", log_controller.get_log_by_id);
-router.get("/api/logs/:statusCode", log_controller.get_logs_by_status_codes);
-router.get("/api/logs/:url", log_controller.get_logs_by_urls);
+router.get("/api/logs/status/:statusCode", log_controller.get_logs_by_status_codes);
+router.get("/api/logs/url/:url", log_controller.get_logs_by_urls);
 
 module.exports = router;
