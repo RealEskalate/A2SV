eb1a361ce850b31d97c3e466a18fc9758797a19f
yiseboge <Ethiopia1!>
2020-04-25 02:36:18 +0300

    [frontend] Implement a skeleton Chart for data display

--- covid-19-app-web/src/views/Home/Statistics.vue
+++ covid-19-app-web/src/views/Home/Statistics.vue
@@ -1,8 +1,25 @@
 <template>
   <section class="statistics">
-    <!--    TODO Replace code here-->
     <h1 class="text-center font-weight-thin">
       Statistics Graph Goes here
     </h1>
+
+    <display />
+    <country-compare />
+    <disease-compare />
   </section>
 </template>
+
+<script>
+import Display from "./Charts/Display.vue";
+import CountryCompare from "./Charts/CountryCompare.vue";
+import DiseaseCompare from "./Charts/DiseaseCompare.vue";
+
+export default {
+  components: {
+    Display,
+    CountryCompare,
+    DiseaseCompare
+  }
+};
+</script>
