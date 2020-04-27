f2b312a38833d8f97052440b53b76b35c11bc72c
Abela <abeltsg32@gmail.com>
2020-04-27 03:01:12 +0300

    Project structure renewed and up to date files uploaded

--- COVID19_Mobile_Application/components/sign-in-page/SignIn.js
+++ COVID19_Mobile_Application/components/sign-in-page/SignIn.js
@@ -8,21 +8,22 @@ import {
   AsyncStorage,
   Alert,
 } from "react-native";
-import { Actions } from "react-native-router-flux";
-import MainPage from "../main-page/MainPage.js";
+import userIDStore from "../data-management/user-id-data/userIDStore";
+import * as actions from "../data-management/user-id-data/userIDActions";
+
 export default class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userName: "",
       password: "",
-      userId: "",
       invalidName: false,
       invalidPassword: false,
       userNameWarning: "Enter your user name",
       passwordWarning: "Enter your password",
     };
   }
+
   isLoggedin() {
     this.loadInitialState().done();
   }
@@ -39,12 +40,10 @@ export default class SignIn extends Component {
   saveUserData = (userID) => {
     AsyncStorage.setItem("userID", userID);
   };
-
-  sendUserId = async () => {
-    let id = await AsyncStorage.getItem("userID");
-    this.props.navigation.navigate("Page 6", {
-      name: "Page 6",
-      userId: id,
+  //navigates to the next page
+  navigateToNextPage = async () => {
+    this.props.navigation.navigate("Main page", {
+      name: "Page 10",
     });
   };
 
@@ -64,8 +63,10 @@ export default class SignIn extends Component {
       })
         .then((response) => response.json())
         .then((json) => {
-          this.saveUserData(json.user._id);
-          this.sendUserId();
+          userIDStore.dispatch(
+            actions.addUser(json.user._id, json.user.username)
+          ); //storing the user id in redux data store
+          this.navigateToNextPage(); //nagivating to the next page
         })
         .catch((error) => {
           Alert.alert(
@@ -151,7 +152,7 @@ export default class SignIn extends Component {
     );
   }
 }
-
+//style of each components on this page
 const styles = StyleSheet.create({
   container: {
     marginTop: 200,
--- COVID19_Mobile_Application/components/symptom-page/SymptomPage.js
+++ COVID19_Mobile_Application/components/symptom-page/SymptomPage.js
@@ -1,29 +1,53 @@
 import React, { Component } from "react";
 import {
+  Image,
   ScrollView,
-  View,
-  Text,
   StyleSheet,
   TouchableOpacity,
+  Alert,
 } from "react-native";
-import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
-import CheckBox from "@react-native-community/checkbox";
+import userIDStore from "../data-management/user-id-data/userIDStore";
+import symptomStore from "../data-management/user-symptom-data/symptomStore";
+import * as symptomActions from "../data-management/user-symptom-data/symptomActions";
+import { CheckBox, Icon } from "react-native-elements";
 
 export default class SymptomPage extends Component {
-  constructor({ route, navigation }) {
-    super({ route, navigation });
+  constructor(props) {
+    super(props);
     this.state = {
-      //userId: route.params.userId,
       symptomId: "",
       symptoms: [],
+      userSymptoms: [],
     };
   }
+
   componentDidMount() {
-    this.fetchData();
+    this.fetchSymptoms();
+    this.fetchUserSymptoms(userIDStore.getState().userId);
+    let interval = setInterval(() => {
+      this.fetchData();
+    }, 1000);
   }
-  //gets the list of symptoms from database
   fetchData() {
-    let newThis = this; // create variable for referencing this
+    this.setState({ userSymptoms: symptomStore.getState() });
+  }
+
+  //registers symptom on database and also stores in local data store
+  handleSymptomAction(userId, symptomId) {
+    let listSymptoms = symptomStore.getState();
+
+    let symptom = listSymptoms.find((obj) => obj.symptom_id == symptomId);
+
+    if (symptom != null) {
+      this.removeSymptom(userId, symptom._id, symptomId);
+    } else {
+      this.registerSymptom(userId, symptomId);
+    }
+  }
+
+  //gets the list of symptoms from database
+  fetchSymptoms() {
+    let newThis = this; // create variable for referencing 'this'
     fetch("http://34.70.173.73:3000/api/symptoms", {
       method: "GET",
       headers: {
@@ -33,18 +57,37 @@ export default class SymptomPage extends Component {
     })
       .then((response) => response.json())
       .then((json) => {
-        // change this
+        // fetching symptoms from the database is successful and storing in to our state
         newThis.setState(() => ({
           symptoms: json,
         }));
-        console.log(json);
+        //console.log(json);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  //gets the list of symptoms from database
+  fetchUserSymptoms(userId) {
+    let newThis = this; // create variable for referencing 'this'
+    fetch("http://34.70.173.73:3000/api/symptomuser/user/" + userId, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        // fetching user symptoms from the database is successful and updating local state using redux
+        symptomStore.dispatch(symptomActions.addSymptom(json));
       })
       .catch((error) => {
         console.log(error);
       });
   }
   //Registers symptom with the user id
-  sendData(userId, symptomId) {
+  registerSymptom(userId, symptomId) {
     fetch("http://34.70.173.73:3000/api/symptomuser", {
       method: "POST",
       headers: {
@@ -58,26 +101,67 @@ export default class SymptomPage extends Component {
     })
       .then((response) => response.json())
       .then((json) => {
-        alert("Success!");
+        // symptom registeration is successful
+        this.fetchUserSymptoms(userIDStore.getState().userId); // get the update from database and update local state
+        //console.log(symptomStore.getState());
       })
+
       .catch((error) => {
         Alert.alert("Oops :(", "Couldn't resgister, please try again!");
       });
   }
+
+  //removes symptom with the user id
+  removeSymptom(userId, randomId, symptomId) {
+    fetch("http://34.70.173.73:3000/api/symptomuser", {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        _id: randomId,
+        symptom_id: symptomId,
+        user_id: userId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        // symptom removal is successful
+        this.fetchUserSymptoms(userIDStore.getState().userId); // get the update from database and update local state
+        //console.log(json);
+      })
+      .catch((error) => {
+        Alert.alert("Oops :(", "Couldn't remove, please try again!");
+      });
+  }
+
+  //check if a symptom has already been registered by user
+  doesSymptomAlreadyRegistered(symptomId) {
+    let symptom = this.state.userSymptoms.find(
+      (obj) => obj.symptom_id == symptomId
+    );
+
+    if (symptom != null) {
+      return true;
+    }
+    return false;
+  }
+
   contents = () =>
     this.state.symptoms.map((item) => {
-      // return the corresponding mapping for each item
+      //return the corresponding mapping for each item in corresponding UI componenets.
       return (
         item &&
         item._id &&
         item.name && (
-          <TouchableOpacity
-          //onPress={() => this.sendData(this.state.userId, item._id)}
-          >
-            <Card key={item._id} style={styles.container}>
-              <Text>{item.name}</Text>
-            </Card>
-          </TouchableOpacity>
+          <CheckBox
+            title={item.name}
+            checked={this.doesSymptomAlreadyRegistered(item._id)}
+            onPress={() =>
+              this.handleSymptomAction(userIDStore.getState().userId, item._id)
+            }
+          />
         )
       );
     });
