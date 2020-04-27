c49cbaa5732cf87935663aab1464eebb5fccf908
yiseboge <Ethiopia1!>
2020-04-27 03:04:35 +0300

    [frontend] Add Disease Comparison Graph

--- covid-19-app-web/src/views/Home/Charts/DiseaseCompare.vue
+++ covid-19-app-web/src/views/Home/Charts/DiseaseCompare.vue
@@ -1,4 +1,45 @@
 <template>
-  <v-container> </v-container>
+  <v-container class="py-8">
+    <v-row class="mx-md-5" dense>
+      <v-col cols="12">
+        <bar-chart
+          :height="650"
+          :chart-data="diseaseData"
+          :options="chartOptions"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-<script></script>
+<script>
+import { BarChart, ChartMixin } from "./charts.js";
+import store from "@/store/index.js";
+
+export default {
+  components: { BarChart },
+  mixins: [ChartMixin],
+  data() {
+    return {
+      diseases: [
+        { name: "Corona", color: [121, 134, 203] },
+        { name: "SARS", color: [77, 208, 225] },
+        { name: "MERS", color: [240, 98, 146] }
+      ],
+    };
+  },
+  methods: {
+    fetchData() {
+      store.dispatch("setDiseaseCompare", {
+        diseases: this.diseases,
+        makeDataSet: this.makeDataSet
+      });
+    }
+  },
+  mounted() {
+    this.fetchData();
+  },
+  computed: {
+    diseaseData: () => store.getters.getDiseaseCompare
+  }
+};
+</script>
