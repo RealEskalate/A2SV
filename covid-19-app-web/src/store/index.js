795112c3ab4bffa7dcd736cd661cbe87f9bacef2
TripleThreads <programmers2017@outlook.com>
2020-04-28 00:12:01 +0300

    [web] states vuex and domo

--- covid-19-app-web/src/store/index.js
+++ covid-19-app-web/src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import graphs from "./graphs";
 import heatmap from "./heatmap";
+import covidStates from './covid-states';
 
 Vue.use(Vuex);
 
@@ -11,6 +12,7 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     graphs,
-    heatmap
+    heatmap,
+    covidStates
   }
 });
