ac538e5220f6c82ecd8ca499c372854cf2f9588a
yiseboge <Ethiopia1!>
2020-04-29 16:26:03 +0300

    [frontend] Remove unusable filters from the graphs

--- covid-19-app-web/src/views/Home/Charts/CountryCompare.vue
+++ covid-19-app-web/src/views/Home/Charts/CountryCompare.vue
@@ -13,42 +13,52 @@
         />
       </v-col>
       <v-col cols="12" md="6">
-        <v-menu
-          :close-on-content-click="false"
-          transition="scale-transition"
-          max-width="290px"
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              solo
-              v-model="dateRangeText1"
-              label="Date Range 1"
-              hint="Country 1: From - To"
-              persistent-hint
-              prepend-inner-icon="mdi-calendar"
-              readonly
-              v-on="on"
-            />
-          </template>
-          <v-date-picker
-            :max="maxDate"
-            range
-            no-title
-            v-model="date_range_1"
-            @input="
-              () => {
-                if (
-                  date_range_1.length === 2 &&
-                  date_range_1[0] &&
-                  date_range_1[1]
-                )
-                  fetchData1();
-              }
-            "
-          />
-        </v-menu>
+        <v-select
+          v-model="country_2"
+          :items="countries"
+          label="Country 2"
+          hint="Country 2"
+          persistent-hint
+          solo
+          @input="fetchData2"
+        />
       </v-col>
+      <!--      <v-col cols="12" md="4">-->
+      <!--        <v-menu-->
+      <!--          :close-on-content-click="false"-->
+      <!--          transition="scale-transition"-->
+      <!--          max-width="290px"-->
+      <!--          min-width="290px"-->
+      <!--        >-->
+      <!--          <template v-slot:activator="{ on }">-->
+      <!--            <v-text-field-->
+      <!--              solo-->
+      <!--              v-model="dateRangeText1"-->
+      <!--              label="Date Range 1"-->
+      <!--              hint="Date Range 1"-->
+      <!--              persistent-hint-->
+      <!--              prepend-inner-icon="mdi-calendar"-->
+      <!--              readonly-->
+      <!--              v-on="on"-->
+      <!--            />-->
+      <!--          </template>-->
+      <!--          <v-date-picker-->
+      <!--            range-->
+      <!--            no-title-->
+      <!--            v-model="date_range_1"-->
+      <!--            @input="-->
+      <!--              () => {-->
+      <!--                if (-->
+      <!--                  date_range_1.length === 2 &&-->
+      <!--                  date_range_1[0] &&-->
+      <!--                  date_range_1[1]-->
+      <!--                )-->
+      <!--                  fetchData1();-->
+      <!--              }-->
+      <!--            "-->
+      <!--          />-->
+      <!--        </v-menu>-->
+      <!--      </v-col>-->
       <!--      <v-col cols="12" md="4">-->
       <!--        <v-slider-->
       <!--          :color="sliderColor(social_distancing_1)"-->
@@ -126,56 +136,6 @@
       <!--      </v-col>-->
     </v-row>
     <v-row class="mx-md-5" dense>
-      <v-col cols="12" md="6">
-        <v-select
-          v-model="country_2"
-          :items="countries"
-          label="Country 2"
-          hint="Country 2"
-          persistent-hint
-          solo
-          @input="fetchData2"
-        />
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-menu
-          :close-on-content-click="false"
-          transition="scale-transition"
-          max-width="290px"
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              solo
-              v-model="dateRangeText2"
-              label="Date Range 2"
-              hint="Country 2: From - To"
-              persistent-hint
-              prepend-inner-icon="mdi-calendar"
-              readonly
-              v-on="on"
-            />
-          </template>
-          <v-date-picker
-            :max="maxDate"
-            range
-            no-title
-            v-model="date_range_2"
-            @input="
-              () => {
-                if (
-                  date_range_2.length === 2 &&
-                  date_range_2[0] &&
-                  date_range_2[1]
-                )
-                  fetchData2();
-              }
-            "
-          />
-        </v-menu>
-      </v-col>
-    </v-row>
-    <v-row class="mx-md-5" dense>
       <v-col cols="12">
         <v-select
           v-model="criterion"
@@ -211,7 +171,11 @@
     </v-row>
     <v-row>
       <v-col cols="12" md="12">
-        <line-chart :height="400" :chart-data="data" :options="chartOptions" />
+        <line-chart
+          :height="400"
+          :chart-data="countriesData"
+          :options="chartOptions"
+        />
       </v-col>
       <!--      <v-col cols="12" md="3">-->
       <!--        <v-card flat tile>-->
@@ -275,15 +239,19 @@ export default {
       criterion: "Confirmed Cases",
       date_range_1: [
         moment(new Date())
-          .subtract(3, "month")
+          .subtract(1, "month")
           .format("YYYY-MM-DD"),
-        moment(new Date()).format("YYYY-MM-DD")
+        moment(new Date())
+          .add(1, "week")
+          .format("YYYY-MM-DD")
       ],
       date_range_2: [
         moment(new Date())
-          .subtract(3, "month")
+          .subtract(1, "month")
           .format("YYYY-MM-DD"),
-        moment(new Date()).format("YYYY-MM-DD")
+        moment(new Date())
+          .add(1, "week")
+          .format("YYYY-MM-DD")
       ],
       country_1: "World",
       country_2: "World",
@@ -293,64 +261,27 @@ export default {
     };
   },
   methods: {
-    labels() {
-      const start =
-        this.date_range_1[0] < this.date_range_2[0]
-          ? this.date_range_1[0]
-          : this.date_range_2[0];
-      const end =
-        this.date_range_1[1] > this.date_range_2[1]
-          ? this.date_range_1[1]
-          : this.date_range_2[1];
-      let result = [];
-      for (let i = 0; i < moment(end).diff(moment(start), "days"); i++) {
-        result.push(`Day ${i}`);
-      }
-      return result;
-    },
-    fillGraph() {
-      console.log("graph updating");
-      let input1 = {
-        label: this.country_1,
-        color: [121, 134, 203],
-        data: this.countriesData.one
-      };
-      let input2 = {
-        label: this.country_2,
-        color: [255, 213, 79],
-        data: this.countriesData.two
-      };
-      this.data = {
-        labels: this.labels(),
-        datasets: [this.makeDataSet(input1), this.makeDataSet(input2)]
-      };
+    fetchCountryResources() {
+      store.dispatch("setCountryResources", {
+        country: this.country
+      });
     },
     fetchData1() {
       store.dispatch("setCountryCompare", {
         country: this.country_1,
         criteria: this.criterion,
+        makeDataSet: this.makeDataSet,
         mode: "one",
-        start_date:
-          this.date_range_1[0] ||
-          moment(new Date())
-            .subtract(3, "month")
-            .format("YYYY-MM-DD"),
-        end_date:
-          this.date_range_1[1] || moment(new Date()).format("YYYY-MM-DD")
+        color: [121, 134, 203]
       });
     },
     fetchData2() {
       store.dispatch("setCountryCompare", {
         country: this.country_2,
         criteria: this.criterion,
+        makeDataSet: this.makeDataSet,
         mode: "two",
-        start_date:
-          this.date_range_2[0] ||
-          moment(new Date())
-            .subtract(3, "month")
-            .format("YYYY-MM-DD"),
-        end_date:
-          this.date_range_2[1] || moment(new Date()).format("YYYY-MM-DD")
+        color: [255, 213, 79]
       });
     }
   },
@@ -358,21 +289,7 @@ export default {
     this.fetchData1();
     this.fetchData2();
   },
-  watch: {
-    countriesData: {
-      deep: true,
-      handler() {
-        this.fillGraph();
-      }
-    }
-  },
   computed: {
-    dateRangeText1() {
-      return this.rangeToText(this.date_range_1[0], this.date_range_1[1]);
-    },
-    dateRangeText2() {
-      return this.rangeToText(this.date_range_2[0], this.date_range_2[1]);
-    },
     countriesData: () => store.getters.getCountryCompare,
     criteriaList() {
       let result = [];
