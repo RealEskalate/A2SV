23692a592d6281ee1eb819b588a802e19b9ad69b
Abela <abeltsg32@gmail.com>
2020-05-02 14:50:44 +0300

    [mobile] changed the type of data sent to backendwhen user signs up

--- COVID19_Mobile_Application/components/sign-up-page/SignUp.js
+++ COVID19_Mobile_Application/components/sign-up-page/SignUp.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Button,
@@ -8,19 +8,19 @@ import {
   TouchableOpacity,
   Picker,
   Alert,
-} from 'react-native';
+} from "react-native";
 export default class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: '',
-      password: '',
-      age: '',
-      gender: '',
+      userName: "",
+      password: "",
+      age: "",
+      gender: "",
       invalidName: false,
       invalidPassword: false,
-      userNameWarning: 'Username should be between 3 - 50 characters',
-      passwordWarning: 'Password should be morethan 5 characters',
+      userNameWarning: "Username should be between 3 - 50 characters",
+      passwordWarning: "Password should be morethan 5 characters",
     };
   }
 
@@ -31,16 +31,16 @@ export default class SignIn extends Component {
       !this.state.invalidPassword &&
       this.state.userName.length > 3 &&
       this.state.password.length >= 6 &&
-      this.state.gender !== 'Select your gender' &&
+      this.state.gender !== "Select your gender" &&
       this.state.gender.length !== 0 &&
-      this.state.age !== 'Select your age group' &&
+      this.state.age !== "Select your age group" &&
       this.state.age.length !== 0
     ) {
-      fetch('https://sym-track.herokuapp.com/api/auth/register', {
-        method: 'POST',
+      fetch("https://sym-track.herokuapp.com/api/auth/register", {
+        method: "POST",
         headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           username: this.state.userName,
@@ -51,30 +51,30 @@ export default class SignIn extends Component {
       })
         .then((response) => response.json())
         .then((json) => {
-          this.props.navigation.navigate('SignIn');
+          this.props.navigation.navigate("SignIn");
         })
         .catch((error) => {
           Alert.alert(
-            'Poor connection',
+            "Poor connection",
             "Couldn't resgister, please try again!"
           );
         });
     } else if (
-      this.state.gender === 'Select your gender' ||
+      this.state.gender === "Select your gender" ||
       this.state.gender.length === 0 ||
-      this.state.age === 'Select your age group' ||
+      this.state.age === "Select your age group" ||
       this.state.age.length === 0 ||
       this.state.userName.length === 0 ||
       this.state.password.length === 0
     ) {
       Alert.alert(
-        'Incomplete information',
-        'Your registeration is incomplete, please enter all the necessary data!'
+        "Incomplete information",
+        "Your registeration is incomplete, please enter all the necessary data!"
       );
     } else {
       Alert.alert(
-        'Invalid Credentials',
-        'You have entered an invalid user name or password. Please try again!'
+        "Invalid Credentials",
+        "You have entered an invalid user name or password. Please try again!"
       );
     }
   };
@@ -107,56 +107,65 @@ export default class SignIn extends Component {
       <View style={styles.container}>
         <Text style={styles.signInTitle}>Sign up</Text>
         <Picker
-          placeholder='Select your gender'
+          placeholder="Select your gender"
           selectedValue={this.state.gender}
-          style={{ height: 50, width: 300, color: '#0a77aa' }}
+          style={{ height: 50, width: 300, color: "#0a77aa" }}
           onValueChange={(itemValue, itemIndex) =>
             this.setState({
               gender: itemValue,
             })
-          }>
-          <Picker.Item label='Select your gender' value='' />
-          <Picker.Item label='Male' value='MALE' />
-          <Picker.Item label='Female' value='Female' />
+          }
+        >
+          <Picker.Item label="Select your gender" value="" />
+          <Picker.Item label="Male" value="MALE" />
+          <Picker.Item label="Female" value="Female" />
         </Picker>
 
         <Picker
           selectedValue={this.state.age}
-          style={{ height: 50, width: 300, color: '#0a77aa' }}
+          style={{ height: 50, width: 300, color: "#0a77aa" }}
           onValueChange={(itemValue, itemIndex) =>
             this.setState({
               age: itemValue,
             })
-          }>
-          <Picker.Item label='Select your age range' value='' />
-          <Picker.Item label='Under 30' value='UNDER_30' />
-          <Picker.Item label='31 to 45' value='31_To_45' />
-          <Picker.Item label='Above 45' value='ABOVE_45' />
+          }
+        >
+          <Picker.Item label="Select your age range" value="" />
+          <Picker.Item label="0 - 10" value="0-10" />
+          <Picker.Item label="11 - 20" value="11-20" />
+          <Picker.Item label="21 - 30" value="21-30" />
+          <Picker.Item label="31 - 40" value="31-40" />
+          <Picker.Item label="41 - 50" value="41-50" />
+          <Picker.Item label="51 - 60" value="51-60" />
+          <Picker.Item label="61 - 70" value="61-70" />
+          <Picker.Item label="71 - 80" value="71-80" />
+          <Picker.Item label="81 - 90" value="81-90" />
+          <Picker.Item label="Greater than 90" value=">90" />
         </Picker>
 
         <TextInput
           style={styles.inputBox}
           onChangeText={(text) => this.validateUserName(text)}
-          placeholder='User name'
-          placeholderTextColor='#0a77aa'
-          selectionColor='#0a77aa'
-          keyboardType='userName-address'
+          placeholder="User name"
+          placeholderTextColor="#0a77aa"
+          selectionColor="#0a77aa"
+          keyboardType="userName-address"
         />
         <Text style={styles.warning}>
-          {this.state.invalidName ? this.state.userNameWarning : ''}
+          {this.state.invalidName ? this.state.userNameWarning : ""}
         </Text>
 
         <TextInput
           style={styles.inputBox}
           onChangeText={(text) => this.validatePassword(text)}
-          placeholder='Password'
+          placeholder="Password"
           secureTextEntry={true}
-          placeholderTextColor='#0a77aa'
+          placeholderTextColor="#0a77aa"
           ref={(input) => (this.password = input)}
           // onSubmitEditing={this.validatePassword(this.state.password)}
         />
         <Text style={styles.warning}>
-          {this.state.invalidPassword ? this.state.passwordWarning : ''}
+          {this.state.invalidPassword ? this.state.passwordWarning : ""}
         </Text>
 
         <TouchableOpacity style={styles.button} onPress={this.signUp}>
@@ -170,53 +179,53 @@ export default class SignIn extends Component {
 const styles = StyleSheet.create({
   container: {
     marginTop: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   inputBox: {
     width: 300,
-    backgroundColor: '#eeeeee',
+    backgroundColor: "#eeeeee",
     borderRadius: 25,
     paddingHorizontal: 16,
     fontSize: 16,
-    color: '#0a77aa',
+    color: "#0a77aa",
     marginVertical: 10,
     padding: 10,
   },
   button: {
     width: 300,
-    backgroundColor: '#0a77aa',
+    backgroundColor: "#0a77aa",
     borderRadius: 25,
     marginVertical: 10,
     paddingVertical: 12,
   },
   buttonText: {
     fontSize: 16,
-    fontWeight: '500',
-    color: '#ffffff',
-    textAlign: 'center',
+    fontWeight: "500",
+    color: "#ffffff",
+    textAlign: "center",
   },
   signInTitle: {
     fontSize: 30,
-    color: '#0a77aa',
+    color: "#0a77aa",
   },
   signupTextCont: {
     flexGrow: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
+    justifyContent: "center",
+    alignItems: "flex-end",
     paddingVertical: 16,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   signupText: {
     fontSize: 16,
   },
   signupButton: {
-    color: '#12799f',
+    color: "#12799f",
     fontSize: 16,
-    fontWeight: '500',
+    fontWeight: "500",
   },
   warning: {
-    color: '#f21509',
+    color: "#f21509",
     fontSize: 12,
   },
 });
