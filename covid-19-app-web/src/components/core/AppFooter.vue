257a87a124e9e7af0008a707f9846f9350e79679
eyosias5 <gemmechumohammed@gmail.com>
2020-05-05 10:35:42 +0300

    [Web] start footer

--- covid-19-app-web/src/components/core/AppFooter.vue
+++ covid-19-app-web/src/components/core/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
   <v-footer class="mt-10 py-5 primary" dark>
-    <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    <span class="px-4">A &copy; {{ new Date().getFullYear() }}</span>
   </v-footer>
 </template>
