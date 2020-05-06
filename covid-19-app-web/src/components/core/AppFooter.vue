5a71a02b44368f4982a354e16deace86408bee9d
eyosias5 <gemmechumohammed@gmail.com>
2020-05-06 09:56:56 +0300

    [Web] implement footer with list

--- covid-19-app-web/src/components/core/AppFooter.vue
+++ covid-19-app-web/src/components/core/AppFooter.vue
@@ -1,5 +1,49 @@
 <template>
-  <v-footer class="mt-10 py-5 primary" dark>
-    <span class="px-4">A &copy; {{ new Date().getFullYear() }}</span>
+  <v-footer class="primary">
+    <div class="col-lg-12 text-center">
+      <v-row class="py-4 text-center">
+        <v-col class="col-lg-2 mx-4">
+          <v-list-item two-line>
+            <v-icon>mdi-youtube</v-icon>
+            <v-list-item-content>
+              <v-list-item-title>Two-line item</v-list-item-title>
+              <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+        <v-col class="col-lg-2 mx-4">
+          <v-list-item two-line>
+            <v-icon>mdi-youtube</v-icon>
+            <v-list-item-content>
+              <v-list-item-title>Two-line item</v-list-item-title>
+              <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+
+      <v-divider></v-divider>
+    </div>
+    <v-row>
+      <v-col class="col-lg-3 white--text">
+        Copyright  <strong>A2SV</strong> {{ new Date().getFullYear() }}
+      </v-col>
+      <v-col class="col-lg-6 text-center">
+        <v-btn v-for="icon in icons" :key="icon" class="mx-2 white--text" icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-col>
+      <v-col class="col-lg-3">
+        <span>asdf</span>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-youtube", "mdi-telegram", "mdi-linkedin"]
+  })
+};
+</script>
