d67e70c95090d88abafbd974d629a032b32c7b3a
eyosias5 <gemmechumohammed@gmail.com>
2020-05-06 12:57:42 +0300

    [Web] footer implement 4 column layout

--- covid-19-app-web/src/components/core/AppFooter.vue
+++ covid-19-app-web/src/components/core/AppFooter.vue
@@ -1,49 +1,102 @@
 <template>
-  <v-footer class="primary">
-    <div class="col-lg-12 text-center">
-      <v-row class="py-4 text-center">
-        <v-col class="col-lg-2 mx-4">
-          <v-list-item two-line>
-            <v-icon>mdi-youtube</v-icon>
-            <v-list-item-content>
-              <v-list-item-title>Two-line item</v-list-item-title>
-              <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-col>
-        <v-col class="col-lg-2 mx-4">
-          <v-list-item two-line>
-            <v-icon>mdi-youtube</v-icon>
-            <v-list-item-content>
-              <v-list-item-title>Two-line item</v-list-item-title>
-              <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-col>
-      </v-row>
-
+  <v-footer class="shadow-lg v-sheet v-sheet--tile theme--light primary">
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs3 md3>
+          <img src="/img/footer/logo.png" style="width: 170px;" />
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-list-item-group color="primary">
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Single-line item</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Single-line item</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Single-line item</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-list-item-group color="primary">
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Single-line item</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Single-line item</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>Single-line item</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-col class="text-center">
+        <v-btn v-for="icon in icons" :key="icon" class="mx-2 white--text" icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-col>
+        </v-flex>
+      </v-layout>
       <v-divider></v-divider>
-    </div>
+      <v-layout row wrap>
+        <v-col class="text-center white--text">
+        Copyright &copy; <strong>A2SV</strong> {{ new Date().getFullYear() }}
+      </v-col>
+      </v-layout>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+
+/***
+ * <v-row>
+      <v-col class="col-lg-3">
+        <img src="/img/footer/logo.png" style="width: 170px;" />
+      </v-col>
+      <v-col class="col-lg-3">
+        <v-list>
+          <v-list-item-content>
+            <v-list-item-title text="anndf"></v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-content>
+            <v-list-item-title text="anndf"></v-list-item-title>
+          </v-list-item-content>
+        </v-list>
+      </v-col>
+      <v-col class="col-lg-3">ll</v-col>
+      <v-col class="col-lg-3">ll</v-col>
+    </v-row>
+
     <v-row>
       <v-col class="col-lg-3 white--text">
-        Copyright  <strong>A2SV</strong> {{ new Date().getFullYear() }}
+        Copyright &copy; <strong>A2SV</strong> {{ new Date().getFullYear() }}
       </v-col>
       <v-col class="col-lg-6 text-center">
         <v-btn v-for="icon in icons" :key="icon" class="mx-2 white--text" icon>
           <v-icon size="24px">{{ icon }}</v-icon>
         </v-btn>
       </v-col>
-      <v-col class="col-lg-3">
-        <span>asdf</span>
-      </v-col>
+      <v-col class="col-lg-3"> </v-col>
     </v-row>
-  </v-footer>
-</template>
-
-<script>
+ */
 export default {
   data: () => ({
-    icons: ["mdi-youtube", "mdi-telegram", "mdi-linkedin"]
+    icons: ["mdi-youtube", "mdi-telegram", "mdi-web"]
   })
 };
 </script>
