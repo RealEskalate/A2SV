cfb6d028be1fbc7d1ac5178529b8de28dbaa4011
Khalid-Sultan <Kalid.sultan.95@gmail.com>
2020-05-09 22:44:22 +0300

    [api] First Attempt at fixing broken statistics api

---
+++ Hack/Backend/models/TestModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const testsSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    country: {
+        type: String,
+        required: true,
+    },
+    country_slug: {
+        type: String,
+        required: true,
+    },
+    tests: {
+        type: Number
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+});
+
+testsSchema.index({ country_slug: 1, date: 1 })
+
+const Tests = mongoose.model("Tests", testsSchema);
+exports.Tests = Tests;
