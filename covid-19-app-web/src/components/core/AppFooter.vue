84b0fbafcec5f70761efee789cde375cd8bd3ea1
Segni Habulu <programmers2017@outlook.com>
2020-05-11 02:00:05 +0300

    footer update (#141)

--- covid-19-app-web/src/components/core/AppFooter.vue
+++ covid-19-app-web/src/components/core/AppFooter.vue
@@ -1,98 +1,85 @@
 <template>
   <v-footer
-    class="shadow-lg v-sheet v-sheet--tile theme--light"
-    style="background: url('https://lh5.googleusercontent.com/proxy/nAm2UTt8kjupZFGWD4iPSIYLhJ7ZyhjHTSkzi-4kEzNBhI0NNx1RvJYSh89lgahLNWnp2Wii51RyzYTuxvHibai7tlpBI68_RLauPSwuSyl_abO2bNSfSkQeopmtBKiaMNlifi9aTkiC')"
+          class="shadow-lg v-sheet v-sheet--tile theme--light"
+          style="background: url('https://lh5.googleusercontent.com/proxy/nAm2UTt8kjupZFGWD4iPSIYLhJ7ZyhjHTSkzi-4kEzNBhI0NNx1RvJYSh89lgahLNWnp2Wii51RyzYTuxvHibai7tlpBI68_RLauPSwuSyl_abO2bNSfSkQeopmtBKiaMNlifi9aTkiC');"
   >
     <v-container class="my-2">
       <v-layout row wrap>
-        <v-flex xs3 md3>
-          <router-link class="d-flex align-center no-decoration" to="/">
-            <v-img
-              alt="Vuetify Logo"
-              class="shrink mr-2"
-              contain
-              src="@/assets/logo-v1.png"
-              transition="scale-transition"
-              width="100"
-            />
-            <div>
-              <h3 class="blue--text no-decoration">Covid-19</h3>
-            </div>
-          </router-link>
-          <span class="font-weight-bold ml-4">Stay Safe</span>
+        <v-flex xs12 md3 class="mx-2 sm12">
+          <p class="font-weight-bold ml-3">Get our App</p>
+          <v-divider class="mx-1"></v-divider>
+          <v-row>
+            <v-col cols="6">
+              <router-link to="/" target="blank">
+                <v-img class="v-card--raised" src="/img/footer/app-store.svg" max-width="10.5em"></v-img>
+              </router-link>
+            </v-col>
+            <v-col cols="6">
+              <router-link to="/" target="blank">
+                <v-img class="v-card--raised" src="/img/footer/google-play.svg" max-width="10.5em"></v-img>
+              </router-link>
+            </v-col>
+          </v-row>
         </v-flex>
-        <v-flex xs4 md3 class="mx-2 text-center">
-          <p class="font-weight-bold">Get our App</p>
-          <v-divider class="mx-4"></v-divider>
-          <v-col class="align-center">
-            <router-link to="/" target="blank">
-              <v-img src="/img/footer/kkk.png" max-width="10.5em"></v-img>
-            </router-link>
-          </v-col>
-          <v-col>
-            <router-link to="/" target="blank">
-              <v-img src="/img/footer/njn.png" max-width="10.5em"></v-img>
-            </router-link>
-          </v-col>
-        </v-flex>
-        <v-flex xs4 md3 class="mx-4">
+        <v-flex xs12 md6 class="mx-4">
           <p class="font-weight-bold text-center">About</p>
           <v-divider class="mx-4"></v-divider>
           <v-col>
-            <span class="">
+            <span class="text-justify">
               Our App is a non-commercial app that uses crowd-sourcing to
               collect and visualize the density of the relevant Covid-19
               symptoms.
-              <br />
+              <br/>
               In addition it provide Play-ground for the Data and News related
               to Covid-19 by country.
             </span>
           </v-col>
         </v-flex>
 
-        <v-flex xs4 md2 class="mx-2">
+        <v-flex xs12 md2 class="mx-2">
           <p class="font-weight-bold text-center">More info</p>
           <v-divider class="mx-4"></v-divider>
           <v-col class="text-center">
             <v-btn
-              v-for="icon in icons"
-              :key="icon"
-              class="mx-2 black--text"
-              icon
-              v-bind:href="icon[1]"
-              target="blank"
+                    v-for="icon in icons"
+                    :key="icon"
+                    class="mx-2 black--text"
+                    icon
+                    v-bind:href="icon[1]"
+                    target="blank"
             >
               <v-icon size="24px">{{ icon[0] }}</v-icon>
             </v-btn>
           </v-col>
-          <v-col class="text-center black--text">
-            Copyright &copy; <strong>A2SV</strong>
-            {{ new Date().getFullYear() }}
-          </v-col>
+          <router-link class="text-center d-block mx-auto" to="about#input-204">Contact us</router-link>
         </v-flex>
       </v-layout>
+      <div class="text-center black--text mb-12 mb-md-0">
+        Copyright &copy; <strong>A2SV</strong>
+        {{ new Date().getFullYear() }}
+      </div>
     </v-container>
   </v-footer>
 </template>
 
 <script>
-export default {
-  data: () => ({
-    icons: [
-      ["mdi-youtube", "/youtube"],
-      ["mdi-telegram", "/telegram"],
-      ["mdi-web", "https://a2sv.org"]
-    ]
-    //https://www.ctech.co.ke/wp-content/uploads/2019/09/Home-Page_Layer-Slider-BG-1024x426.png
-    //https://www.toptal.com/designers/subtlepatterns/patterns/memphis-mini.png
-    //https://www.toptal.com/designers/subtlepatterns/patterns/memphis-mini.png
-    //https://www.designbolts.com/wp-content/uploads/2012/12/Triangle-White-Seamless-Patterns.jpg
-  })
-};
+  export default {
+    data: () => ({
+      icons: [
+        ["mdi-youtube", "/youtube"],
+        ["mdi-telegram", "/telegram"],
+        ["mdi-web", "https://a2sv.org"]
+      ]
+      //https://www.ctech.co.ke/wp-content/uploads/2019/09/Home-Page_Layer-Slider-BG-1024x426.png
+      //https://www.toptal.com/designers/subtlepatterns/patterns/memphis-mini.png
+      //https://www.toptal.com/designers/subtlepatterns/patterns/memphis-mini.png
+      //https://www.designbolts.com/wp-content/uploads/2012/12/Triangle-White-Seamless-Patterns.jpg
+    })
+  };
 </script>
 
 <style scoped>
-.list {
-  background: none;
-}
+  .list {
+    background: none;
+  }
 </style>
