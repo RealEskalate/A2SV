42a955326f1ec7f02ecce4dc4a178c9497effb9b
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-05-11 14:19:48 +0300

    [security] adding authentication for location and location user routes

--- Hack/Backend/routes/MedicalHistoryUserRoutes.js
+++ Hack/Backend/routes/MedicalHistoryUserRoutes.js
@@ -7,7 +7,7 @@ const medicalhistory_user_controller = require("../controllers/MedicalHistoryUse
 router.get("/api/medicalhistoryuser/", verifyToken.verifyToken, medicalhistory_user_controller.get_all_medicalhistory_user);
 router.get("/api/medicalhistoryuser/:id", verifyToken.verifyToken, medicalhistory_user_controller.get_all_medicalhistory_user_by_id);
 router.get("/api/medicalhistoryuser/user/:user_id", verifyToken.verifyToken, medicalhistory_user_controller.get_medicalhistory_user_by_user_id);
-router.get("/api/medicalhistoryuser/medicalhistory/:medicalhistory_id", medicalhistory_user_controller.get_medicalhistory_user_by_medicalhistory_id);
+router.get("/api/medicalhistoryuser/medicalhistory/:medicalhistory_id", verifyToken.verifyToken, medicalhistory_user_controller.get_medicalhistory_user_by_medicalhistory_id);
 
 router.post("/api/medicalhistoryuser/", verifyToken.verifyToken, medicalhistory_user_controller.post_medicalhistory_user);
 router.patch("/api/medicalhistoryuser/", verifyToken.verifyToken, medicalhistory_user_controller.update_medicalhistory_user);
--- Hack/Backend/routes/MessageRoutes.js
+++ Hack/Backend/routes/MessageRoutes.js
@@ -1,14 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const verifyToken = require("../middlewares/auth.js");
 
 const MessageController = require("../controllers/MessageController.js");
 
-router.get("/api/messages/", verifyToken.verifyToken, MessageController.getAllMessages);
-router.get("/api/messages/email/:email", verifyToken.verifyToken, MessageController.getMessageByEmail);
-router.get("/api/messages/:id", verifyToken.verifyToken, MessageController.getMessageById);
+router.get("/api/messages/", MessageController.getAllMessages);
+router.get("/api/messages/email/:email", MessageController.getMessageByEmail);
+router.get("/api/messages/:id", MessageController.getMessageById);
 
-router.post("/api/messages", verifyToken.verifyToken, MessageController.postMessage);
-router.delete("/api/messages/", verifyToken.verifyToken, MessageController.deleteMessages);
+router.post("/api/messages", MessageController.postMessage);
+router.delete("/api/messages/", MessageController.deleteMessages);
 
 module.exports = router;
