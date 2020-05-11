04f79f9c1e82d8b443e4369ac858b8355f5d089a
yiseboge <Ethiopia1!>
2020-05-11 09:22:38 +0300

    [web] Fix Shadow bug in states

--- covid-19-app-web/src/views/Information/States.vue
+++ covid-19-app-web/src/views/Information/States.vue
@@ -10,7 +10,7 @@
       <v-row>
         <v-col cols="12" md="7" class="pl-md-12 py-md-9 px-8 py-5">
           <img-svg
-            class="col-12 py-md-12 px-md-12 px-5 py-3 grey lighten-4 v-card--shaped in-shadow"
+            class="col-12 py-md-12 px-md-12 px-5 py-3 grey lighten-4 v-card--shaped shadow-in"
           />
         </v-col>
         <v-col cols="12" md="5" class="pr-md-12 py-md-8 px-8 py-5">
