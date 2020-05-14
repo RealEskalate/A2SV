83b02a1149ae4711feec048ebb61e346641fffe8
TripleThreads <programmers2017@outlook.com>
2020-05-14 20:37:32 +0300

    compression added

--- covid-19-app-web/server.js
+++ covid-19-app-web/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const compression = require("compression");
 const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
+app.use(compression());
 app.get(/.*/, function(req, res) {
   res.sendfile(__dirname + "/dist/index.html");
 });
