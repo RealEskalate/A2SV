1bde8b8e748c5e6b3bb3cd398e9556759bd4e882
TripleThreads <programmers2017@outlook.com>
2020-05-14 21:31:41 +0300

    compression finally worked halelujah

--- covid-19-app-web/server.js
+++ covid-19-app-web/server.js
@@ -3,8 +3,8 @@ const compression = require("compression");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + "/dist/"));
 app.use(compression());
+app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
   res.sendfile(__dirname + "/dist/index.html");
 });
