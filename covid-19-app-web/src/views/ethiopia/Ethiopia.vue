6dfdb0c33243048a8255815903af519aa6247563
TripleThreads <programmers2017@outlook.com>
2020-05-19 11:26:11 +0300

    local ethiopian data

---
+++ covid-19-app-web/src/views/ethiopia/Ethiopia.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-card class="col-md-3 mx-2">
+        <v-card-title class="h1">
+          Total cases
+          335
+        </v-card-title>
+      </v-card>
+      <v-card class="col-md-3 mx-2">
+        <v-card-title class="h1">
+          Recovered
+          335
+        </v-card-title>
+      </v-card>
+
+      <v-card class="col-md-3 mx-2">
+        <v-card-title class="h1">
+          Death
+          335
+        </v-card-title>
+      </v-card>
+
+      <v-card class="col-md-3 mx-2">
+        <v-card-title class="h1">
+          Active cases
+          335
+        </v-card-title>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: "Ethiopia"
+  }
+</script>
+
+<style scoped>
+  .col-md-3 {
+    max-width: 22%;
+  }
+</style>
