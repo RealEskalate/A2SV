5906b8d842712c2ca2f729fe860278b8f63d6a90
TripleThreads <programmers2017@outlook.com>
2020-05-23 23:39:38 +0300

    first time lang selection bug

--- covid-19-app-web/vue.config.js
+++ covid-19-app-web/vue.config.js
@@ -1,6 +1,10 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
+  chainWebpack(config) {
+    config.plugins.delete('prefetch');
+    config.plugin('CompressionPlugin').use(CompressionPlugin);
+  },
   configureWebpack: {
     plugins: [new CompressionPlugin()],
     resolve: {
