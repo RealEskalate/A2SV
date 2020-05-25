eec147d6cde1ece9228e596538c8dcbe174a5150
feyselmubarek <feyselmubarek@gmail.com>
2020-05-25 15:01:26 +0300

    [mobile] about page refreshed using UI kitten

--- Track Sym Mobile App/src/pages/about-page/About.js
+++ Track Sym Mobile App/src/pages/about-page/About.js
@@ -1,43 +1,39 @@
-import React from "react";
+import React from 'react';
 import {
   View,
   TouchableOpacity,
   Image,
   Dimensions,
   Linking,
-} from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import Text from "./CustomText.js";
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import {
+  Layout,
+  Text,
+  StyleService,
+  useStyleSheet,
+  Button,
+} from '@ui-kitten/components';
+import { Divider } from 'react-native-paper';
 
 const About = (props) => {
+  const styles = useStyleSheet(themedStyle);
+
   return (
-    <ScrollView style={{ backgroundColor: "#fff" }}>
-      <View style={{ height: 230 }}>
+    <ScrollView style={{ backgroundColor: '#fff' }}>
+      <Layout level='2' style={{ height: 220 }}>
         <Image
-          source={require("../../../assets/images/tracker.png")}
-          style={{ height: 220, width: Dimensions.get("screen").width }}
-          resizeMode="cover"
+          source={require('../../../assets/images/track.png')}
+          style={{ height: 220, width: Dimensions.get('screen').width }}
+          resizeMode='cover'
         />
-      </View>
-      <View style={{ paddingLeft: 10, paddingRight: 10 }}>
-        <Text
-          style={{
-            fontSize: 18,
-            color: "#37474f",
-            fontFamily: "Roboto-Black",
-          }}
-        >
-          TRACK SYM
-        </Text>
-        <Text
-          style={{
-            color: "#455a64",
-            marginTop: 10,
-            marginBottom: 10,
-            fontSize: 13,
-            textAlign: "justify",
-          }}
-        >
+      </Layout>
+      <Layout>
+        <Layout style={styles.title_container}>
+          <Text category='h5'>TRACK SYM</Text>
+        </Layout>
+        <Divider />
+        <Text style={styles.text}>
           It is a non-commercial COVID-19 symptom tracking app that uses
           crowd-sourcing to collect and visualize the density of the relevant
           symptoms. Users can anonymously report their symptoms and choose a
@@ -46,83 +42,63 @@ const About = (props) => {
           visiting a grocery store, a gas station, or any other place that is
           heavily used by symptomatic people
         </Text>
-      </View>
-      <View style={{ height: 230 }}>
+      </Layout>
+      <Layout level='2' style={{ height: 230 }}>
         <Image
-          source={require("../../../assets/images/team.jpg")}
-          style={{ height: 230, width: Dimensions.get("screen").width }}
-          resizeMode="cover"
+          source={require('../../../assets/images/team.png')}
+          style={{ height: 230, width: Dimensions.get('screen').width }}
+          resizeMode='cover'
         />
-      </View>
-      <View style={{ paddingLeft: 10, paddingRight: 10 }}>
-        <Text
-          style={{
-            fontSize: 18,
-            color: "#37474f",
-            fontFamily: "Roboto-Black",
-          }}
-        >
-          WHO ARE WE?
-        </Text>
-        <Text
-          style={{
-            color: "#455a64",
-            marginTop: 10,
-            marginBottom: 10,
-            fontSize: 13,
-            textAlign: "justify",
-          }}
-        >
+      </Layout>
+      <Layout>
+        <Layout style={styles.title_container}>
+          <Text category='h5'>WHO ARE WE ?</Text>
+        </Layout>
+        <Divider />
+        <Text style={styles.text}>
           A2SV - Africa to Silicon Valley is a team of highly motivated and
           talented students from Ethiopia, led by an ex-Google and Palantir
           Software/ML engineer.
         </Text>
-      </View>
-      <View style={{ height: 220 }}>
+      </Layout>
+      <Layout level='2' style={{ height: 220 }}>
         <Image
-          source={require("../../../assets/images/file.jpg")}
-          style={{ height: 230, width: Dimensions.get("screen").width }}
-          resizeMode="cover"
+          source={require('../../../assets/images/file.png')}
+          style={{ height: 230, width: Dimensions.get('screen').width }}
+          resizeMode='cover'
         />
-      </View>
-      <View style={{ padding: 10 }}>
-        <Text
-          style={{
-            fontSize: 18,
-            color: "#37474f",
-            fontFamily: "Roboto-Black",
-          }}
-        >
-          YOUR DATA
-        </Text>
-        <Text
-          style={{
-            color: "#455a64",
-            marginTop: 10,
-            fontSize: 13,
-            textAlign: "justify",
-          }}
-        >
+      </Layout>
+      <Layout>
+        <Layout style={styles.title_container}>
+          <Text category='h5'>YOUR DATA</Text>
+        </Layout>
+        <Divider />
+        <Text style={styles.text}>
           Your data will be used anonymously for the purpose of data science and
           statistics, meaning any info generated isn’t traced back to a single
           user. This is a non-commercial project with no intention of profit.
         </Text>
-        <TouchableOpacity
-          style={{
-            backgroundColor: "#bbdefb",
-            padding: 5,
-            marginTop: 10,
-            justifyContent: "center",
-            alignItems: "center",
-            borderRadius: 5,
-          }}
-          onPress={() => Linking.openURL("http://www.a2sv.org")}
-        >
-          <Text>CHECK OUT OUR WEBSITE</Text>
-        </TouchableOpacity>
-      </View>
+      </Layout>
+      <Layout style={{ flex: 1 }}>
+        <Layout style={styles.bottom}>
+          <Button onPress={() => Linking.openURL('http://www.a2sv.org')}>
+            CHECK OUT OUR WEBSITE
+          </Button>
+        </Layout>
+      </Layout>
     </ScrollView>
   );
 };
 
 export default About;
+
+const themedStyle = StyleService.create({
+  title_container: { margin: 5, alignItems: 'center' },
+  text: {
+    padding: 10,
+  },
+  bottom: {
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+});
