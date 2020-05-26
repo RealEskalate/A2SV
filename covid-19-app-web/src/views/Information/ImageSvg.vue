8d1e22285207a1984a9770030437e4462963bd59
TripleThreads <programmers2017@outlook.com>
2020-05-26 15:19:38 +0300

    localization and bug fixed

--- covid-19-app-web/src/views/Information/ImageSvg.vue
+++ covid-19-app-web/src/views/Information/ImageSvg.vue
@@ -80,13 +80,13 @@
                 style="box-sizing: border-box; font-size: 0; text-align: center; "
               >
                 <div
-                  style="display: inline-block; font-size: 12px; font-family: Roboto; color: #fff; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
+                        style="display: inline-block; font-size: 12px; font-family: Nunito; color: #fff; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
                 >
                   <span
                     id="docs-internal-guid-866c777f-7fff-93b4-6baa-9a2faff79d20"
                     ><span
                       style="font-size: 11pt ; font-weight: 700 ; vertical-align: baseline"
-                      >MILD OR NO SYMPTOMS</span
+                  >{{ $t('states.mildOrNoSymptom') }} </span
                     ></span
                   >
                 </div>
@@ -169,13 +169,13 @@
                 style="box-sizing: border-box; font-size: 0; text-align: center; "
               >
                 <div
-                  style="display: inline-block; font-size: 12px; font-family: Roboto; color: #fff; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
+                        style="display: inline-block; font-size: 12px; font-family: Nunito; color: #fff; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
                 >
                   <span
                     id="docs-internal-guid-8132d71f-7fff-2e0f-6b61-d2264aad7718"
                     ><span
                       style="font-size: 11pt ; font-weight: 700 ; vertical-align: baseline"
-                      >HEAVY SYMPTOMS</span
+                  >{{ $t('states.heavySymptoms') }} </span
                     ></span
                   >
                 </div>
@@ -183,12 +183,12 @@
             </div>
           </foreignObject>
           <text
-            x="611"
-            y="394"
-            fill="#000000"
-            font-family="Roboto"
-            font-size="12px"
-            text-anchor="middle"
+                  x="611"
+                  y="394"
+                  fill="#000000"
+                  font-family="Nunito"
+                  font-size="12px"
+                  text-anchor="middle"
           >
             Heavy symptoms
           </text>
@@ -268,13 +268,13 @@
                 style="box-sizing: border-box; font-size: 0; text-align: center; "
               >
                 <div
-                  style="display: inline-block; font-size: 12px; font-family: Roboto; color: #fff; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
+                        style="display: inline-block; font-size: 12px; font-family: Nunito; color: #fff; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
                 >
                   <span
                     id="docs-internal-guid-252f80f8-7fff-d25d-9b1a-71f1ec292fb8"
                     ><span
                       style="font-size: 10pt ; font-weight: 700 ; vertical-align: baseline"
-                      >HOSPITALIZATION</span
+                  >{{ $t('states.hospitalization') }} </span
                     ></span
                   >
                 </div>
@@ -282,12 +282,12 @@
             </div>
           </foreignObject>
           <text
-            x="336"
-            y="634"
-            fill="#fff"
-            font-family="Roboto"
-            font-size="12px"
-            text-anchor="middle"
+                  x="336"
+                  y="634"
+                  fill="#fff"
+                  font-family="Nunito"
+                  font-size="12px"
+                  text-anchor="middle"
           >
             Hospitalization
           </text>
@@ -351,13 +351,13 @@
                 style="box-sizing: border-box; font-size: 0; text-align: center; "
               >
                 <div
-                  style="display: inline-block; font-size: 12px; font-family: Comic Sans MS; color: #000000; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
+                        style="display: inline-block; font-size: 12px; font-family: Nunito; color: #000000; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
                 >
                   <span
                     id="docs-internal-guid-0a722c08-7fff-1658-3a6b-8dd855d43165"
                     ><span
                       style="font-size: 12pt ; font-weight: 700 ; vertical-align: baseline"
-                      ><font color="#ffffff">ICU</font></span
+                  ><font color="#ffffff">{{ $t('states.icu') }} </font></span
                     ></span
                   >
                 </div>
@@ -365,12 +365,12 @@
             </div>
           </foreignObject>
           <text
-            x="81"
-            y="394"
-            fill="#000000"
-            font-family="Roboto"
-            font-size="12pt"
-            text-anchor="middle"
+                  x="81"
+                  y="394"
+                  fill="#000000"
+                  font-family="Nunito"
+                  font-size="12pt"
+                  text-anchor="middle"
           >
             ICU
           </text>
@@ -402,33 +402,33 @@
                 style="box-sizing: border-box; font-size: 0; text-align: center; "
               >
                 <div
-                  style="display: inline-block; font-size: 12pt; font-family: Roboto, sans-serif; color: #000000; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
+                        style="display: inline-block; font-size: 12pt; font-family: Nunito, sans-serif; color: #000000; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
                 >
-                  <b><font color="#ffffff">DEATH</font></b>
+                  <b><font color="#ffffff">{{ $t('death') }}</font></b>
                 </div>
               </div>
             </div>
           </foreignObject>
           <text
-            x="336"
-            y="394"
-            fill="#000000"
-            font-family="Comic Sans MS"
-            font-size="12px"
-            text-anchor="middle"
+                  x="336"
+                  y="394"
+                  fill="#000000"
+                  font-family="Nunito"
+                  font-size="12px"
+                  text-anchor="middle"
           >
             DEATH
           </text>
         </switch>
       </g>
       <ellipse
-        cx="441"
-        cy="40"
-        rx="60"
-        ry="40"
-        :fill="good_color"
-        stroke="none"
-        pointer-events="all"
+              cx="441"
+              cy="40"
+              rx="60"
+              ry="40"
+              :fill="main_color"
+              stroke="none"
+              pointer-events="all"
       />
       <g transform="translate(-0.5 -0.5)">
         <switch>
@@ -447,20 +447,20 @@
                 style="box-sizing: border-box; font-size: 0; text-align: center; "
               >
                 <div
-                  style="display: inline-block; font-size: 12pt; font-family: Roboto, sans-serif; color: #000000; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
+                        style="display: inline-block; font-size: 12pt; font-family: Nunito, sans-serif; color: #000000; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
                 >
-                  <b><font color="#ffffff">RECOVERY</font></b>
+                  <b><font color="#ffffff">{{ $t('states.recovery') }}</font></b>
                 </div>
               </div>
             </div>
           </foreignObject>
           <text
-            x="441"
-            y="44"
-            fill="#000000"
-            font-family="Comic Sans MS"
-            font-size="12px"
-            text-anchor="middle"
+                  x="441"
+                  y="44"
+                  fill="#000000"
+                  font-family="Nunito"
+                  font-size="12px"
+                  text-anchor="middle"
           >
             RECOVERY
           </text>
@@ -508,13 +508,13 @@
                 style="box-sizing: border-box; font-size: 0; text-align: center;"
               >
                 <div
-                  style="display: inline-block; font-size: 12px; font-family: Lucida Console; color: #fff; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
+                        style="display: inline-block; font-size: 12px; font-family: Nunito, sans-serif; color: #fff; line-height: 1.2; pointer-events: all; white-space: normal; word-wrap: normal; "
                 >
                   <span
                     id="docs-internal-guid-bbfade3a-7fff-70cb-7443-5425a3b80c55"
                     ><span
-                      style="font-size: 11pt ; font-weight: 700 ; vertical-align: baseline; font-family: Roboto, sans-serif"
-                      >INCUBATION</span
+                          style="font-size: 11pt ; font-weight: 700 ; vertical-align: baseline; font-family: Nunito, sans-serif"
+                  >{{ $t('states.incubation') }} </span
                     ></span
                   >
                 </div>
@@ -522,12 +522,12 @@
             </div>
           </foreignObject>
           <text
-            x="241"
-            y="44"
-            fill="#000000"
-            font-family="Roboto"
-            font-size="12px"
-            text-anchor="middle"
+                  x="241"
+                  y="44"
+                  fill="#000000"
+                  font-family="Nunito"
+                  font-size="12px"
+                  text-anchor="middle"
           >
             Incubation
           </text>
@@ -550,9 +550,9 @@
 </template>
 
 <script>
-import store from "@/store/";
+  import store from "@/store/";
 
-export default {
+  export default {
   name: "ImageSvg",
   data() {
     return {
