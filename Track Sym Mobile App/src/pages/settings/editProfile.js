67d8680b08682c2be1f22eb7a2b09130b998cb46
feyselmubarek <feyselmubarek@gmail.com>
2020-05-28 12:00:12 +0300

    [moble] username modal issue resolved

--- Track Sym Mobile App/src/pages/settings/editProfile.js
+++ Track Sym Mobile App/src/pages/settings/editProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import {
   Button,
   Divider,
@@ -15,39 +15,35 @@ import {
   Modal,
   Card,
   Text,
-} from "@ui-kitten/components";
-import {
-  SafeAreaView,
-  View,
-  TouchableWithoutFeedback,
-  AsyncStorage,
-} from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { ProfileAvatar } from "./extra/profile-avatar.component";
-import userIDStore from "../../data-management/user-id-data/userIDStore";
-import * as actions from "../../data-management/user-id-data/userIDActions";
+} from '@ui-kitten/components';
+import { SafeAreaView, View, TouchableWithoutFeedback } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { ProfileAvatar } from './extra/profile-avatar.component';
+import userIDStore from '../../data-management/user-id-data/userIDStore';
+import * as actions from '../../data-management/user-id-data/userIDActions';
+import { KeyboardAvoidingView } from '../../components/3rd-party';
 
-const ArrowIosBackIcon = (style) => <Icon {...style} name="arrow-ios-back" />;
+const ArrowIosBackIcon = (style) => <Icon {...style} name='arrow-ios-back' />;
 const data = [
-  "Update your age group",
-  "0-10",
-  "11-20",
-  "21-30",
-  "31-40",
-  "41-50",
-  "51-60",
-  "61-70",
-  "71-80",
-  "81-90",
-  ">90",
+  'Update your age group',
+  '0-10',
+  '11-20',
+  '21-30',
+  '31-40',
+  '41-50',
+  '51-60',
+  '61-70',
+  '71-80',
+  '81-90',
+  '>90',
 ];
 
-const genderData = ["Update your gender", "MALE", "FEMALE"];
+const genderData = ['Update your gender', 'MALE', 'FEMALE'];
 
 const EditProfileScreen = (props) => {
-  const [username, setUsername] = React.useState("");
-  const [usernameCap, setUsernameCap] = React.useState("");
-  const [usernameStatus, setUsernameStatus] = React.useState("basic");
+  const [username, setUsername] = React.useState('');
+  const [usernameCap, setUsernameCap] = React.useState('');
+  const [usernameStatus, setUsernameStatus] = React.useState('basic');
   const [isLoading, setIsLoading] = React.useState(false);
   const [selectedAgeIndex, setSelectedAgeIndex] = React.useState(
     new IndexPath(0)
@@ -56,10 +52,21 @@ const EditProfileScreen = (props) => {
     new IndexPath(0)
   );
 
+  const getMyUsername = async () => {
+    try {
+      let userName = await AsyncStorage.getItem('userName');
+      setUsername(userName);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getMyUsername();
+  }, []);
+
   const displayAgeValue = data[selectedAgeIndex.row];
   const [modalState, setModalState] = React.useState(false);
-  const [modalMessage, setModalMessage] = React.useState("");
-  const [modalStatus, setModalStatus] = React.useState("");
+  const [modalMessage, setModalMessage] = React.useState('');
+  const [modalStatus, setModalStatus] = React.useState('');
 
   const renderOption = (title, index) => (
     <SelectItem title={title} key={index} />
@@ -71,12 +78,12 @@ const EditProfileScreen = (props) => {
   );
 
   const onUserNameChange = (name) => {
-    if (name !== "") {
-      setUsernameStatus("basic");
-      setUsernameCap("");
+    if (name !== '') {
+      setUsernameStatus('basic');
+      setUsernameCap('');
     } else {
-      setUsernameStatus("danger");
-      setUsernameCap("required*");
+      setUsernameStatus('danger');
+      setUsernameCap('required*');
     }
     setUsername(name);
   };
@@ -90,9 +97,9 @@ const EditProfileScreen = (props) => {
 
   const onGenderChange = (pass) => {
     if (pass === 0) {
-      setModalMessage("Please select your gender");
+      setModalMessage('Please select your gender');
       setModalState(true);
-      setModalStatus("danger");
+      setModalStatus('danger');
       return;
     }
     setSelectedGenderIndex(pass);
@@ -100,9 +107,9 @@ const EditProfileScreen = (props) => {
 
   const onAgeGroupChange = (pass) => {
     if (pass === 0) {
-      setModalMessage("Please enter your age group");
+      setModalMessage('Please enter your age group');
       setModalState(true);
-      setModalStatus("danger");
+      setModalStatus('danger');
       return;
     }
 
@@ -111,26 +118,24 @@ const EditProfileScreen = (props) => {
 
   const renderPassIcon = (props) => (
     <TouchableWithoutFeedback
-      onPress={() => setCurrPasswordVisible(!currPasswordVisible)}
-    >
-      <Icon {...props} name={currPasswordVisible ? "eye-off" : "eye"} />
+      onPress={() => setCurrPasswordVisible(!currPasswordVisible)}>
+      <Icon {...props} name={currPasswordVisible ? 'eye-off' : 'eye'} />
     </TouchableWithoutFeedback>
   );
 
   const renderNewPassIcon = (props) => (
     <TouchableWithoutFeedback
-      onPress={() => setNewPasswordVisible(!newPasswordVisible)}
-    >
-      <Icon {...props} name={newPasswordVisible ? "eye-off" : "eye"} />
+      onPress={() => setNewPasswordVisible(!newPasswordVisible)}>
+      <Icon {...props} name={newPasswordVisible ? 'eye-off' : 'eye'} />
     </TouchableWithoutFeedback>
   );
 
-  const CameraIcon = (style) => <Icon {...style} name="camera" />;
+  const CameraIcon = (style) => <Icon {...style} name='camera' />;
 
   const renderPhotoButton = () => (
     <Button
       style={styles.editAvatarButton}
-      status="basic"
+      status='basic'
       accessoryLeft={CameraIcon}
     />
   );
@@ -139,14 +144,14 @@ const EditProfileScreen = (props) => {
     onUserNameChange(username);
     onAgeGroupChange(selectedAgeIndex);
     onGenderChange(selectedGenderIndex);
-    if (selectedAgeIndex > 0 && selectedGenderIndex > 0 && username !== "") {
+    if (selectedAgeIndex > 0 && selectedGenderIndex > 0 && username !== '') {
       setIsLoading(true);
-      fetch("https://sym-track.herokuapp.com/api/users", {
-        method: "PATCH",
+      fetch('https://sym-track.herokuapp.com/api/users', {
+        method: 'PATCH',
         headers: {
-          Authorization: "Bearer " + userIDStore.getState().userToken,
-          Accept: "application/json",
-          "Content-Type": "application/json",
+          Authorization: 'Bearer ' + userIDStore.getState().userToken,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           _id: userIDStore.getState().userId,
@@ -157,10 +162,10 @@ const EditProfileScreen = (props) => {
       })
         .then((response) => response.json())
         .then((json) => {
-          setModalMessage("You have successfully changed your profile!");
+          setModalMessage('You have successfully changed your profile!');
           setModalState(true);
           setIsLoading(false);
-          setModalStatus("success");
+          setModalStatus('success');
           saveUser(
             username,
             data[selectedAgeIndex],
@@ -180,15 +185,15 @@ const EditProfileScreen = (props) => {
           console.log(error);
           setModalMessage("Oops, couldn't update your profile! Please retry!");
           setModalState(true);
-          setModalStatus("danger");
+          setModalStatus('danger');
         });
     }
   };
   const saveUser = async (userName, age_group, gender) => {
     try {
-      await AsyncStorage.setItem("userName", userName); //save user name on async storage
-      await AsyncStorage.setItem("age_group", age_group); //save age group on async storage
-      await AsyncStorage.setItem("gender", gender); //save gender on async storage
+      await AsyncStorage.setItem('userName', userName); //save user name on async storage
+      await AsyncStorage.setItem('age_group', age_group); //save age group on async storage
+      await AsyncStorage.setItem('gender', gender); //save gender on async storage
     } catch (error) {
       console.log(error);
     }
@@ -199,71 +204,67 @@ const EditProfileScreen = (props) => {
       <Modal
         visible={modalState}
         backdropStyle={styles.backdrop}
-        onBackdropPress={() => setModalState(false)}
-      >
+        onBackdropPress={() => setModalState(false)}>
         <Card disabled={true} style={{ marginLeft: 10, marginRight: 10 }}>
-          <Text status={modalStatus} category="h6" style={{ marginBottom: 10 }}>
+          <Text status={modalStatus} category='h6' style={{ marginBottom: 10 }}>
             {modalMessage}
           </Text>
           <Text
-            style={{ alignSelf: "flex-end", color: "#0080ff" }}
+            style={{ alignSelf: 'flex-end', color: '#0080ff' }}
             onPress={() => {
               setModalState(false);
-              setUsername("");
-              setSelectedAgeIndex("");
-              setSelectedGenderIndex("");
+              setUsername('');
+              setSelectedAgeIndex('');
+              setSelectedGenderIndex('');
               setIsLoading(false);
-            }}
-          >
+            }}>
             Dismiss
           </Text>
         </Card>
       </Modal>
       <TopNavigation
-        alignment="center"
-        title="CHANGE PROFILE"
+        alignment='center'
+        title='CHANGE PROFILE'
         accessoryLeft={renderBackAction}
       />
       <Divider />
       <Layout style={{ flex: 1 }}>
-        <KeyboardAwareScrollView>
-          <Layout level="2" style={{ padding: 10 }}>
+        <KeyboardAvoidingView>
+          <Layout level='2' style={{ padding: 10 }}>
             <ProfileAvatar
               style={styles.profileAvatar}
-              source={require("../../../assets/man.png")}
-              editButton={renderPhotoButton}
+              source={require('../../../assets/man.png')}
             />
           </Layout>
           <View style={styles.formContainer}>
             <Input
               placeholder={userIDStore.getState().userName}
-              label="Username"
+              label='Username'
+              disabled={true}
               caption={usernameCap}
               status={usernameStatus}
-              autoCapitalize="words"
+              autoCapitalize='words'
               value={username}
               onChangeText={(name) => onUserNameChange(name)}
             />
             <Select
               style={styles.select}
-              label="AGE GROUP"
+              label='AGE GROUP'
               style={styles.formInput}
-              placeholder="Default"
+              placeholder='Default'
               value={displayAgeValue}
               selectedIndex={selectedAgeIndex}
-              onSelect={(index) => setSelectedAgeIndex(index)}
-            >
+              onSelect={(index) => setSelectedAgeIndex(index)}>
               {data.map((index, title) => renderOption(index, title))}
             </Select>
             <Select
               style={styles.select}
-              label="GENDER"
+              label='GENDER'
               style={styles.formInput}
-              placeholder="Default"
+              placeholder='Default'
               value={displayGenderValue}
               selectedIndex={selectedGenderIndex}
-              onSelect={(index) => setSelectedGenderIndex(index)}
-            >
+              onSelect={(index) => setSelectedGenderIndex(index)}>
               {genderData.map((index, title) =>
                 renderGenderOption(index, title)
               )}
@@ -271,14 +272,13 @@ const EditProfileScreen = (props) => {
           </View>
           <Button
             style={styles.doneButton}
-            size="large"
+            size='large'
             disabled={isLoading}
             accessoryLeft={() => (isLoading ? <Spinner /> : <></>)}
-            onPress={() => updateProfile()}
-          >
+            onPress={() => updateProfile()}>
             DONE
           </Button>
-        </KeyboardAwareScrollView>
+        </KeyboardAvoidingView>
       </Layout>
     </SafeAreaView>
   );
@@ -306,16 +306,16 @@ const styles = StyleService.create({
   profileAvatar: {
     aspectRatio: 1.0,
     height: 124,
-    alignSelf: "center",
+    alignSelf: 'center',
   },
   formInput: {
     marginTop: 16,
   },
   backdrop: {
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   indicator: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
