cfe5d20506a8ec4db49a9260f05ede18648acbff
feyselmubarek <feyselmubarek@gmail.com>
2020-05-28 12:00:42 +0300

    [mobile] terms page added

--- Track Sym Mobile App/src/pages/settings/settingStack.js
+++ Track Sym Mobile App/src/pages/settings/settingStack.js
@@ -1,61 +1,31 @@
-import React from "react";
+import React from 'react';
 import {
   createStackNavigator,
   CardStyleInterpolators,
-} from "@react-navigation/stack";
-import { SettingScreen } from "./settings";
-//import ProfileDetail from '../../../components/profile-page/ProfileDetail';
-import { SafeAreaView } from "react-native";
-import {
-  TopNavigation,
-  TopNavigationAction,
-  Icon,
-  Divider,
-} from "@ui-kitten/components";
-import ChangePassScreen from "./changePassword";
-import EditProfileScreen from "./editProfile";
+} from '@react-navigation/stack';
+import { SettingScreen } from './settings';
+import ChangePassScreen from './changePassword';
+import EditProfileScreen from './editProfile';
+import TermsAndPrivacyScreen from './terms';
 
 const { Navigator, Screen } = createStackNavigator();
-const ArrowIosBackIcon = (style) => <Icon {...style} name="arrow-ios-back" />;
 
 export const SettingNavigator = (props) => {
-  // const ProfileScreen = (props) => {
-  //   const renderBackAction = () => (
-  //     <TopNavigationAction
-  //       icon={ArrowIosBackIcon}
-  //       onPress={props.navigation.goBack}
-  //     />
-  //   );
-
-  //   return (
-  //     <SafeAreaView style={{flex: 1}}>
-  //       <TopNavigation
-  //         title="Edit Profile"
-  //         alignment="center"
-  //         accessoryLeft={renderBackAction}
-  //       />
-  //       <Divider />
-  //       <ProfileDetail />
-  //     </SafeAreaView>
-  //   );
-  // };
-
-  const [initRouteName, setInitRouteName] = React.useState("index");
+  const [initRouteName, setInitRouteName] = React.useState('index');
 
   return (
     <Navigator
-      headerMode="none"
+      headerMode='none'
       initialRouteName={initRouteName}
       screenOptions={{
         gestureEnabled: true,
-        gestureDirection: "horizontal",
+        gestureDirection: 'horizontal',
         cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-      }}
-    >
-      <Screen name="index" component={SettingScreen} />
-      {/* <Screen name="ProfileScreen" component={ProfileScreen} /> */}
-      <Screen name="ChangePassScreen" component={ChangePassScreen} />
-      <Screen name="EditProfileScreen" component={EditProfileScreen} />
+      }}>
+      <Screen name='index' component={SettingScreen} />
+      <Screen name='ChangePassScreen' component={ChangePassScreen} />
+      <Screen name='EditProfileScreen' component={EditProfileScreen} />
+      <Screen name='TermsAndPrivacyScreen' component={TermsAndPrivacyScreen} />
     </Navigator>
   );
 };
