42d6d7160f4560e54ee7663da78ab87a320db94d
TripleThreads <programmers2017@outlook.com>
2020-06-03 14:48:08 +0300

    sym track added

--- covid-19-app-web/src/views/HeatMap/HeatMap.vue
+++ covid-19-app-web/src/views/HeatMap/HeatMap.vue
@@ -3,31 +3,52 @@
     <v-container>
       <v-row>
         <v-col>
-          <v-responsive
-            :aspect-ratio="$vuetify.breakpoint.smAndDown ? 12 / 16 : 16 / 12"
-          >
-            <iframe
-              src="https://public.domo.com/cards/dG1jy"
-              width="100%"
-              height="100%"
-              marginheight="0"
-              marginwidth="0"
-              frameborder="0"
-            ></iframe>
-            <!--            <iframe-->
-            <!--              src="https://public.domo.com/cards/bWxVg"-->
-            <!--              width="100%"-->
-            <!--              height="100%"-->
-            <!--              marginheight="0"-->
-            <!--              marginwidth="0"-->
-            <!--              frameborder="0"-->
-            <!--            />-->
-          </v-responsive>
+          <v-tabs fixed-tabs>
+            <v-tab>
+              Symtrack tab
+            </v-tab>
+            <v-tab>
+              Global map(Legacy)
+            </v-tab>
+            <v-tab-item>
+              <sym-track/>
+            </v-tab-item>
+            <v-tab-item>
+              <v-responsive
+                      :aspect-ratio="
+                  $vuetify.breakpoint.smAndDown ? 12 / 16 : 16 / 12
+                "
+              >
+                <iframe
+                        src="https://public.domo.com/cards/dG1jy"
+                        width="100%"
+                        height="100%"
+                        marginheight="0"
+                        marginwidth="0"
+                        frameborder="0"
+                ></iframe>
+                <!--            <iframe-->
+                <!--              src="https://public.domo.com/cards/bWxVg"-->
+                <!--              width="100%"-->
+                <!--              height="100%"-->
+                <!--              marginheight="0"-->
+                <!--              marginwidth="0"-->
+                <!--              frameborder="0"-->
+                <!--            />-->
+              </v-responsive>
+            </v-tab-item>
+          </v-tabs>
         </v-col>
       </v-row>
     </v-container>
   </section>
 </template>
 <script>
-export default {};
+  import SymTrack from "./Maps/SymTrack";
+
+  export default {
+    components: {
+      SymTrack
+    }
+  };
 </script>
--- covid-19-app-web/src/views/HeatMap/Maps/SymTrack.vue
+++ covid-19-app-web/src/views/HeatMap/Maps/SymTrack.vue
@@ -1,13 +1,11 @@
 <template>
-  $END$
+  <h1>empty symtrack map page</h1>
 </template>
 
 <script>
   export default {
     name: "SymTrack"
-  }
+  };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
