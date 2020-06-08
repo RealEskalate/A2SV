93cf9d06506c0ddd74ea1bad0aa174b6fe0777c2
TripleThreads <programmers2017@outlook.com>
2020-06-09 01:34:18 +0300

    trying for working path

--- covid-19-app-web/server.js
+++ covid-19-app-web/server.js
@@ -4,9 +4,9 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(compression());
-app.use(express.static(__dirname + "./dist/"));
+app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
-  res.sendfile(__dirname + "./dist/index.html");
+  res.sendfile(__dirname + "/dist/index.html");
 });
 app.listen(port);
 
