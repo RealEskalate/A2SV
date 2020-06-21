fd82f584d4f3f7cccb74608d8a477d0fc7248074
TripleThreads <programmers2017@outlook.com>
2020-06-21 16:24:59 +0300

    prerending

--- covid-19-app-web/src/main.js
+++ covid-19-app-web/src/main.js
@@ -7,6 +7,7 @@ import i18n from "./plugins/i18n";
 import VueTour from "vue-tour";
 import VueProgressBar from "vue-progressbar";
 import VTooltip from "v-tooltip";
+import injectInitialState from "./utils/inject-initial-state";
 
 require("vue-tour/dist/vue-tour.css");
 Vue.use(VTooltip);
@@ -42,10 +43,37 @@ Vue.mixin({
   }
 });
 
-new Vue({
+const app = new Vue({
   router,
   store,
   vuetify,
   i18n,
   render: h => h(App)
-}).$mount("#app");
+});
+
+if (window.__INITIAL_STATE__) store.replaceState(window.__INITIAL_STATE__);
+
+router.beforeResolve(async (to, from, next) => {
+  try {
+    const components = router.getMatchedComponents(to);
+
+    // By using `await` we make sure to wait
+    // for the API request made by the `fetch()`
+    // method to resolve before rendering the view.
+    await Promise.all(components.map(x => x.fetch && x.fetch({ store })));
+
+    // The `injectInitialState()` function injects
+    // the current state as a global variable
+    // `__INITIAL_STATE__` if the page is currently
+    // pre-rendered.
+    if (window.__PRERENDER_INJECTED) injectInitialState(store.state);
+  } catch (error) {
+    // This is the place for error handling in
+    // case the API request fails for example.
+    console.log(error);
+  }
+
+  return next();
+});
+
+app.$mount("#app");
--- covid-19-app-web/vue.config.js
+++ covid-19-app-web/vue.config.js
@@ -1,11 +1,52 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 const webpack = require("webpack");
+
+const path = require("path");
+const PrerenderSpaPlugin = require("prerender-spa-plugin");
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, "dist"),
+    routes: [
+      "/",
+      "/en",
+      "/en/about",
+      "/en/news",
+      "/en/map",
+      "/en/information",
+      "/am",
+      "/am/about",
+      "/am/news",
+      "/am/map",
+      "/am/information",
+      "/ao",
+      "/ao/about",
+      "/ao/news",
+      "/ao/map",
+      "/ao/information"
+    ],
+    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+      // We need to inject a value so we're able to
+      // detect if the page is currently pre-rendered.
+      inject: {},
+      // Our view component is rendered after the API
+      // request has fetched all the necessary data,
+      // so we create a snapshot of the page after the
+      // `data-view` attribute exists in the DOM.
+      maxConcurrentRoutes: 1
+    })
+  })
+];
+
 module.exports = {
   runtimeCompiler: true,
 
   chainWebpack(config) {
     config.plugins.delete("prefetch");
     config.plugin("CompressionPlugin").use(CompressionPlugin);
+    if (process.env.NODE_ENV === "production") {
+      config.plugin("prerender").use(...productionPlugins);
+    }
   },
   configureWebpack: {
     plugins: [
