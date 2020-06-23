177d9007c457b0b4be0c1dc393057cd4d891616e
Abela <abeltsg32@gmail.com>
2020-06-23 18:22:32 +0300

    [mobile] backend integeration update on symptom history view

--- Track Sym Mobile App/src/pages/symptom-history/index.js
+++ Track Sym Mobile App/src/pages/symptom-history/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from "react";
 import {
   Layout,
   Text,
@@ -6,41 +6,142 @@ import {
   Divider,
   Icon,
   Spinner,
-} from '@ui-kitten/components';
-import { View, StyleSheet } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+  ListItem,
+} from "@ui-kitten/components";
+import { View, StyleSheet, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-import userIDStore from '../../data-management/user-id-data/userIDStore';
+import userIDStore from "../../data-management/user-id-data/userIDStore";
+import symptomStore from "../../data-management/user-symptom-data/symptomStore";
+import * as symptomActions from "../../data-management/user-symptom-data/symptomActions";
+import languageStore from "../../data-management/language_data/languageStore";
+import { strings } from "../../localization/localization";
+import { LangContext } from "../../../assets/lang/language-context";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+import { ThemeContext } from "../../../assets/themes/theme-context";
+MapboxGL.setAccessToken(
+  "pk.eyJ1IjoiZmVyb3g5OCIsImEiOiJjazg0czE2ZWIwNHhrM2VtY3Y0a2JkNjI3In0.zrm7UtCEPg2mX8JCiixE4g"
+);
 
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const days = [
+  strings.Sun,
+  strings.Mon,
+  strings.Tue,
+  strings.Wed,
+  strings.Thu,
+  strings.Fri,
+  strings.Sat,
+];
 const months = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sem',
-  'Oct',
-  'Nov',
-  'Dec',
+  strings.Jan,
+  strings.Feb,
+  strings.Mar,
+  strings.Apr,
+  strings.May,
+  strings.Jun,
+  strings.Jul,
+  strings.Aug,
+  strings.Sep,
+  strings.Oct,
+  strings.Nov,
+  strings.Dec,
 ];
 
-const SymptomHistory = () => {
-  const [data, setData] = useState([]);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [filteredData, setFilteredData] = useState();
-  const [loading, setLoading] = useState(true);
+export default class SymptomHistory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loading: true,
+      selectedIndex: -1,
+      filteredData: [],
+      currLanguage: "English",
+      user_longitude: 0.0,
+      user_latitude: 0.0,
+      currLangCode: languageStore.getState(),
+      currUserSymptoms: [],
+      currSelected: true,
+      calendar: [],
+    };
 
-  const [calander, setCalander] = useState();
+    symptomStore.subscribe(() => {
+      this.fetchUserSymptoms(userIDStore.getState().userId);
+      this.fetchData();
+    });
 
-  const populateCalander = () => {
-    const caladerData = [];
+    languageStore.subscribe(() => {
+      strings.setLanguage(languageStore.getState());
+      this.setState({ currLangCode: languageStore.getState() });
+      this.componentDidMount();
+    });
+
+    this.onUserLocationUpdate = this.onUserLocationUpdate.bind(this);
+  }
+
+  //static contextType = ThemeContext;
+
+  onUserLocationUpdate(location) {
+    this.setState({ user_latitude: location.coords.latitude });
+    this.setState({ user_longitude: location.coords.longitude });
+  }
+  componentDidMount = async () => {
+    await this.setState({ currLangCode: languageStore.getState() });
+    switch (this.state.currLangCode) {
+      case "am":
+        await this.setState({ currLanguage: "Amharic" });
+        break;
+      case "en":
+        await this.setState({ currLanguage: "English" });
+        break;
+      case "orm":
+        await this.setState({ currLanguage: "Oromo" });
+        break;
+      case "tr":
+        await this.setState({ currLanguage: "English" });
+        break;
+    }
+    this.fetchUserSymptoms(userIDStore.getState().userId);
+    this.fetchData();
+    this.populateCalander();
+    this.fetchSymptom();
+
+    this.timer = setInterval(() => {
+      if (this.state.currUserSymptoms.length != 0) {
+        fetch("https://sym-track.herokuapp.com/api/user_locations", {
+          method: "POST",
+          headers: {
+            Authorization: "Bearer " + userIDStore.getState().userToken,
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            longitude: this.state.user_longitude,
+            latitude: this.state.user_latitude,
+            user_id: userIDStore.getState().userId,
+            TTL: 10000,
+          }),
+        })
+          .then((response) => response.json())
+          .then(() => {
+            //console.log(json);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      } else {
+        console.log("No symptoms to report");
+        console.log(
+          this.state.user_latitude + " , " + this.state.user_longitude
+        );
+      }
+    }, 10000);
+  };
+
+  populateCalander = () => {
+    caladerData = [];
 
     for (let index = 0; index < 14; index++) {
-      const currentDate = new Date();
+      currentDate = new Date();
       currentDate.setDate(currentDate.getDate() - index);
 
       caladerData.push({
@@ -51,203 +152,302 @@ const SymptomHistory = () => {
       });
     }
 
-    setCalander(caladerData);
+    this.setState({ calendar: caladerData });
   };
 
-  const colors = {
-    LOW: '#558b2f',
-    MEDIUM: '#fbc02d',
-    HIGH: '#d32f2f',
+  compareDate = (date1, date2) => {
+    return (
+      date1.getDate() <= date2.getDate() &&
+      date1.getMonth() <= date2.getMonth() &&
+      date1.getFullYear() <= date2.getFullYear()
+    );
   };
 
-  useEffect(() => {
-    populateCalander();
-    fetchSymptom();
-  }, []);
+  colors = {
+    LOW: "#558b2f",
+    MEDIUM: "#fbc02d",
+    HIGH: "#d32f2f",
+  };
 
-  const fetchSymptom = async () => {
-    try {
-      const response = await fetch(
-        `https://sym-track.herokuapp.com/api/symptomuserhistory/user/${
-          userIDStore.getState().userId
-        }`,
-        {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${userIDStore.getState().userToken}`,
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+  fetchData() {
+    this.setState({ currUserSymptoms: symptomStore.getState() });
+  }
 
-      if (response.status === 200) {
-        const json = await response.json();
-        setData(json.events);
-
-        json && updateSymptomHistory(0);
+  fetchSymptom = async () => {
+    let newThis = this; // create variable for referencing 'this'
+    fetch(
+      `https://sym-track.herokuapp.com/api/symptomuserhistory/user/${
+        userIDStore.getState().userId
+      }`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + userIDStore.getState().userToken,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
       }
-    } catch (error) {
-      console.log(`sym hist ${error}`);
-    }
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        newThis.setState({
+          data: json.events,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const getParsedDate = (unparsedDate) => {
-    const date = new Date(unparsedDate);
+  getParsedDate = (unparsedDate) => {
+    date = new Date(unparsedDate);
     return `${
       months[date.getMonth()]
     } ${date.getDate()} - ${date.getFullYear()}`;
   };
 
-  const updateSymptomHistory = (dateIndex) => {
-    setSelectedIndex(dateIndex);
-    setLoading(true);
+  updateSymptomHistory = (dateIndex) => {
+    this.setState({
+      loading: true,
+      currSelected: false,
+      selectedIndex: dateIndex,
+    });
 
     let selectedDate = new Date();
-    if (calander) {
-      selectedDate = new Date(calander[dateIndex].compareDate);
+    if (this.state.calendar) {
+      selectedDate = new Date(this.state.calendar[dateIndex].compareDate);
     }
 
-    setFilteredData(
-      data.filter((d) => {
-        return selectedDate.getTime() >= new Date(d.start).getTime();
-      })
-    );
+    this.setState({
+      filteredData: this.state.data.filter((d) => {
+        return (
+          this.compareDate(selectedDate, new Date(d.start)) &&
+          this.compareDate(new Date(d.end), selectedDate)
+        );
+      }),
+    });
 
-    setLoading(false);
+    this.setState({
+      loading: false,
+    });
+  };
+  //If user hasn't registered any symptom
+  emptySymptomList = () => {
+    return (
+      <Layout
+        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      >
+        {/* <MaterialCommunityIcons
+          name="grease-pencil"
+          size={60}
+          color={customTheme.theme === "light" ? "black" : "white"}
+        /> */}
+        <Image
+          style={{ width: 200, height: 250 }}
+          resizeMode="contain"
+          source={require("../../../assets/images/empty.png")}
+        />
+        <Text>{strings.YouHaveNotRegisteredAnySymptom}</Text>
+      </Layout>
+    );
+  };
+  //gets the list of symptoms from database
+  fetchUserSymptoms = (userId) => {
+    //console.log(currLanguage);
+    this.setState({
+      loading: true,
+    });
+    let newThis = this; // create variable for referencing 'this'
+    fetch(
+      "https://sym-track.herokuapp.com/api/symptomuser/user/" +
+        userId +
+        "?language=" +
+        this.state.currLanguage,
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + userIDStore.getState().userToken,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        // fetching user symptoms from the database is successful and updating local state using redux
+        symptomStore.dispatch(symptomActions.updateSymptomList(json));
+        this.setState({
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  return (
-    <Layout style={styles.contanier} level='2'>
-      <Layout style={{ flex: 1 }}>
-        <Layout style={{ flexDirection: 'row' }}>
-          <List
-            horizontal
-            data={calander}
-            inverted
-            showsHorizontalScrollIndicator={false}
-            renderItem={({ item, index }) => (
-              <TouchableOpacity
-                style={{
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                }}
-                onPress={() => {
-                  updateSymptomHistory(index);
-                }}>
-                <Layout
-                  level={selectedIndex === index ? '3' : '1'}
-                  style={{
-                    width: 60,
-                    height: 70,
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  <Text appearance='hint' category='h6'>
-                    {item.day}
-                  </Text>
-                  <Text category='h6' style={{ fontWeight: 'bold' }}>
-                    {days[item.dayIdx]}
-                  </Text>
-                  <Text appearance='hint'>{months[item.monthIdx]}</Text>
-                </Layout>
-              </TouchableOpacity>
-            )}
+  contents = () => (
+    <List
+      data={this.state.currUserSymptoms}
+      renderItem={({ item }) => (
+        <>
+          <ListItem
+            style={{ padding: 5 }}
+            title={item.Symptom.name}
+            description={item.Symptom.description}
           />
-          <TouchableOpacity
-            style={{
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-            onPress={() => {
-              setSelectedIndex(-1);
-            }}>
-            <Layout
-              level={selectedIndex === -1 ? '3' : '1'}
-              style={{
-                width: 60,
-                height: 70,
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Icon
-                style={{ width: 24, height: 24 }}
-                fill='#8F9BB3'
-                name='calendar-outline'
-              />
-              <Text appearance='hint'>Current</Text>
-            </Layout>
-          </TouchableOpacity>
-        </Layout>
-
-        <Divider />
-
-        <Layout level='2' style={{ padding: 10 }}>
-          <Text category='h6' appearance='hint'>
-            MY SYMPTOMS
-          </Text>
-        </Layout>
-        <Divider />
-
+          <Divider />
+        </>
+      )}
+    />
+  );
+  render() {
+    return (
+      <Layout style={styles.contanier} level="2">
         <Layout style={{ flex: 1 }}>
-          {loading ? (
-            <Layout
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Spinner />
-            </Layout>
-          ) : (
+          <Layout style={{ flexDirection: "row" }}>
             <List
-              data={filteredData}
-              renderItem={({ item }) => (
-                <>
+              horizontal
+              data={this.state.calendar}
+              inverted
+              showsHorizontalScrollIndicator={false}
+              renderItem={({ item, index }) => (
+                <TouchableOpacity
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                  onPress={() => {
+                    this.updateSymptomHistory(index);
+                  }}
+                >
                   <Layout
+                    level={this.state.selectedIndex === index ? "3" : "1"}
                     style={{
-                      flexDirection: 'row',
-                      justifyContent: 'space-between',
-                      paddingHorizontal: 20,
-                      paddingVertical: 7,
-                    }}>
-                    <Layout style={{ justifyContent: 'center' }}>
-                      <Text category='h6'>{item.name}</Text>
-                      <Text appearance='hint'>
-                        Registered {getParsedDate(item.start)}
-                      </Text>
-                    </Layout>
-                    <Layout
-                      style={{
-                        width: 50,
-                        height: 40,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                      }}>
-                      <View
-                        style={{
-                          backgroundColor: colors[item.relevance],
-                          width: 30,
-                          height: 30,
-                          borderRadius: 5,
-                        }}
-                      />
-                      <Text appearance='hint'>{item.relevance}</Text>
-                    </Layout>
+                      width: 60,
+                      height: 70,
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text appearance="hint" category="h6">
+                      {item.day}
+                    </Text>
+                    <Text category="h6" style={{ fontWeight: "bold" }}>
+                      {days[item.dayIdx]}
+                    </Text>
+                    <Text appearance="hint">{months[item.monthIdx]}</Text>
                   </Layout>
-                  <Divider />
-                </>
+                </TouchableOpacity>
               )}
             />
-          )}
+            <TouchableOpacity
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={() => {
+                this.setState({
+                  selectedIndex: -1,
+                  currSelected: true,
+                });
+              }}
+            >
+              <Layout
+                level={this.state.selectedIndex === -1 ? "3" : "1"}
+                style={{
+                  width: 60,
+                  height: 70,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Icon
+                  style={{ width: 24, height: 24 }}
+                  fill="#8F9BB3"
+                  name="calendar-outline"
+                />
+                <Text appearance="hint">Current</Text>
+              </Layout>
+            </TouchableOpacity>
+          </Layout>
+
+          <Divider />
+
+          <Layout level="2" style={{ padding: 10 }}>
+            <Text category="h6" appearance="hint">
+              {strings.MySymptoms}
+            </Text>
+          </Layout>
+          <Divider />
+
+          <Layout style={{ flex: 1 }}>
+            {this.state.loading ? (
+              <Layout
+                style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Spinner />
+              </Layout>
+            ) : this.state.currSelected ? (
+              this.state.currUserSymptoms.length === 0 ? (
+                this.emptySymptomList()
+              ) : (
+                this.contents()
+              )
+            ) : this.state.filteredData.length === 0 ? (
+              this.emptySymptomList()
+            ) : (
+              <List
+                data={this.state.filteredData}
+                renderItem={({ item }) => (
+                  <>
+                    <Layout
+                      style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        paddingHorizontal: 20,
+                        paddingVertical: 7,
+                      }}
+                    >
+                      <Layout style={{ justifyContent: "center" }}>
+                        <Text category="h6">{item.name}</Text>
+                        <Text appearance="hint">
+                          Registered {this.getParsedDate(item.start)}
+                        </Text>
+                      </Layout>
+                      <Layout
+                        style={{
+                          width: 50,
+                          height: 40,
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <View
+                          style={{
+                            backgroundColor: this.colors[item.relevance],
+                            width: 30,
+                            height: 30,
+                            borderRadius: 5,
+                          }}
+                        />
+                        <Text appearance="hint">{item.relevance}</Text>
+                      </Layout>
+                    </Layout>
+                    <Divider />
+                  </>
+                )}
+              />
+            )}
+          </Layout>
         </Layout>
       </Layout>
-    </Layout>
-  );
-};
-
-export default SymptomHistory;
-
+    );
+  }
+}
 const styles = StyleSheet.create({
   contanier: { flex: 1 },
 });
