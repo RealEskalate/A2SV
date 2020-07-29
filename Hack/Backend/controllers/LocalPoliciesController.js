eadd4175b2dbf5f2aa711295c4a1ada5bdfb3903
AntenehAdmasu <antenehadmasu8@gmail.com>
2020-07-29 16:06:04 +0300

    get local pollicies for a user implementation

--- Hack/Backend/controllers/LocalPoliciesController.js
+++ Hack/Backend/controllers/LocalPoliciesController.js
@@ -1,21 +1,22 @@
 const { LocalPolicy } = require("./../models/LocalPolicyModel");
+const { User } = require("../models/UserModel");
 
 
 exports.get_local_policies = async (req, res) => {
     let filters = {};
-    if(req.query.country){
+    if (req.query.country) {
         filters.country = req.query.country;
     }
-    if(req.query.city){
+    if (req.query.city) {
         filters.city = req.query.city;
     }
     let page = parseInt(req.query.page) || 1;
     let size = parseInt(req.query.size) || 15;
 
     let policies = await LocalPolicy.find(
-        filters, 
-        {}, 
-        {skip: (page - 1) * size, limit: size * 1});
+        filters,
+        {},
+        { skip: (page - 1) * size, limit: size * 1 });
 
     let result = {
         data_count: await LocalPolicy.countDocuments(filters),
@@ -32,15 +33,39 @@ exports.get_local_policies = async (req, res) => {
 
 exports.get_one_local_policy = async (req, res) => {
     let local_policy = await LocalPolicy.findById(req.params.id);
-    if(!local_policy){
+    if (!local_policy) {
         res.status(500).send("Local Policy Not Found");
-    }else{
+    } else {
         try {
             res.send(local_policy);
         } catch (err) {
             res.status(500).send(err.toString());
         }
-    }    
+    }
+}
+
+exports.get_local_policy_for_user = async (req, res) => {
+    let { last_seen } = req.query
+    let filter = {}
+    // Assuming country must be provided always
+    filter.country = req.query.country
+    if (req.query.city) {
+        filter.city = req.query.city
+    }
+    if (req.query.last_seen) {
+        filter.date_created = { $lt: last_seen }
+    }
+
+    let local_policy = await LocalPolicy.find(filter);
+    if (!local_policy) {
+        res.status(500).send("Local Policy Not Found");
+    } else {
+        try {
+            res.send(local_policy);
+        } catch (err) {
+            res.status(500).send(err.toString());
+        }
+    }
 }
 
 exports.post_local_policy = async (req, res) => {
@@ -50,39 +75,39 @@ exports.post_local_policy = async (req, res) => {
         country: req.body.country,
         city: req.body.city,
         date_terminated: req.body.date_terminated
-      });
-      try {
+    });
+    try {
         await local_policy.save();
         res.status(201).send(local_policy);
-      } catch (err) {
+    } catch (err) {
         res.status(500).send(err.toString());
-      }
+    }
 }
 
 exports.update_local_policy = async (req, res) => { //TODO: Handle not found
     let check = await LocalPolicy.findById(req.body.id);
-    if(!check){
+    if (!check) {
         res.status(404).send("Local Policy Not Found");
-    }else{
+    } else {
         try {
-            let local_policy = await LocalPolicy.findByIdAndUpdate({_id: req.body.id}, req.body, {new: true});
+            let local_policy = await LocalPolicy.findByIdAndUpdate({ _id: req.body.id }, req.body, { new: true });
             res.status(201).send(local_policy);
         } catch (err) {
             res.status(500).send(err.toString());
         }
-    }    
+    }
 }
 
 exports.delete_local_policy = async (req, res) => {
     let check = await LocalPolicy.findById(req.body.id);
-    if(!check){
+    if (!check) {
         res.status(404).send("Local Policy Not Found");
-    }else{
+    } else {
         try {
-            let local_policy = await LocalPolicy.findByIdAndDelete({_id : req.body.id},);
+            let local_policy = await LocalPolicy.findByIdAndDelete({ _id: req.body.id },);
             res.status(201).send(local_policy);
         } catch (err) {
             res.status(500).send(err.toString());
-        } 
-    } 
+        }
+    }
 }
--- Hack/Backend/routes/LocalPolicyRoutes.js
+++ Hack/Backend/routes/LocalPolicyRoutes.js
@@ -4,6 +4,7 @@ var localPoliciesController = require("./../controllers/LocalPoliciesController"
 
 router.get("/api/local_policy", localPoliciesController.get_local_policies);
 router.get("/api/local_policy/:id", localPoliciesController.get_one_local_policy);
+router.get("/api/local_policy/user", localPoliciesController.get_local_policy_for_user)
 router.post("/api/local_policy", localPoliciesController.post_local_policy);
 router.patch("/api/local_policy", localPoliciesController.update_local_policy);
 router.delete("/api/local_policy", localPoliciesController.delete_local_policy);
