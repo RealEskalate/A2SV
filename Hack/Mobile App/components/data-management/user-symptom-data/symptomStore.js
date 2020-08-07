55f3046176e836621186c80d0f4f3419fe642ecb
Biruk Solomon <41226525+Infinidrix@users.noreply.github.com>
2020-08-07 11:24:46 +0300

    [backend] move code from Hack/Backend to Backend (#336)

--- Hack/Mobile App/components/data-management/user-symptom-data/symptomStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import reducer from "./symptomReducers";
-import SymptomPage from "../../symptom-page/SymptomPage";
-
-const symptomStore = createStore(reducer);
-
-export default symptomStore;
