2e3600abbb7638f9d791e9fefb9d62c8662aae63
Segni Habulu <programmers2017@outlook.com>
2020-08-11 14:34:51 +0300

    [web] admin starter files (#345)
    * add initial components
    refactor router
    
    * fix lint
    
    * gitignore updated

---
+++ covid-19-app-web/src/views-admin/auth/CreateAccount.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="title">Register</h1>
+</template>
+
+<script>
+export default {
+  name: "CreateAccount"
+};
+</script>
+
+<style scoped></style>
---
+++ covid-19-app-web/src/views-admin/symptoms/Symptoms.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="title">Symptoms</h1>
+</template>
+
+<script>
+export default {
+  name: "Symptoms"
+};
+</script>
+
+<style scoped></style>
