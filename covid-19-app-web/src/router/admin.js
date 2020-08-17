9085f6da440979454bdecce95202e9713d3a527a
TripleThreads <programmers2017@outlook.com>
2020-08-17 15:12:52 +0300

    add invite admin component

--- covid-19-app-web/src/router/admin.js
+++ covid-19-app-web/src/router/admin.js
@@ -2,6 +2,7 @@ const Dashboard = () => import("../views-admin/dashboard/Dashboard.vue");
 
 const CreateAccount = () => import("../views-admin/auth/CreateAccount.vue");
 const Verify = () => import("../views-admin/auth/Verify.vue");
+const InviteAdmin = () => import("../views-admin/auth/InviteAdmin.vue");
 
 const Symptoms = () => import("../views-admin/symptoms/Symptoms.vue");
 const SymptomDetails = () =>
@@ -11,6 +12,7 @@ export const admin = [
   { name: "CreateAccount", path: "register", component: CreateAccount },
   { name: "Verify", path: "verify", component: Verify },
   { name: "Symptoms", path: "symptoms", component: Symptoms },
+  { name: "InviteAdmin", path: "invite-admin", component: InviteAdmin },
   {
     name: "SymptomDetails",
     path: "symptoms/:id/details",
