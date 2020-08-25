d925460aea6a6ed16f40d3a722069c41aa99b556
Beimnet Zewdu <zbeimnet@gmail.com>
2020-08-25 15:47:07 +0300

    [web] Admin symptom views (#387)
    * [web] Admin symptom view
    
    [web] Admin symptom detail view
    
    * [web] Admin symptom filter
    
    * [web] Admin highlevel stats
    
    * [web] Add symptom in vuex store
    
    * [web] Remove symptom detail route
    
    * [web] Fix DateRangePicker component

---
+++ covid-19-app-web/src/views-admin/symptoms/HighLevelStatistics.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-row>
+    <v-col cols="12" sm="6" md="4">
+      <v-skeleton-loader
+        :loading="getSymptomStatLoaders.total"
+        transition="scale-transition"
+        height="100"
+        type="list-item-two-line"
+      >
+        <v-card class="mx-auto" :flat="true" max-width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">{{ getTotalSymptoms }}</p>
+            <div class="text--primary">{{ title_one }}</div>
+          </v-card-text>
+        </v-card>
+      </v-skeleton-loader>
+    </v-col>
+    <v-col cols="12" sm="6" md="4">
+      <v-skeleton-loader
+        :loading="getSymptomStatLoaders.total"
+        transition="scale-transition"
+        height="100"
+        type="list-item-two-line"
+      >
+        <v-card class="mx-auto" :flat="true" max-width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">
+              {{ getMostCommonSymptom }} |
+              {{
+                Math.round(
+                  ((getMostCommonSymptomCount * 100) / getTotalSymptoms) * 100
+                ) / 100
+              }}%
+            </p>
+            <div class="text--primary">{{ title_two }}</div>
+          </v-card-text>
+        </v-card>
+      </v-skeleton-loader>
+    </v-col>
+    <v-col cols="12" sm="6" md="4">
+      <v-skeleton-loader
+        :loading="getSymptomStatLoaders.totalPeople"
+        transition="scale-transition"
+        height="100"
+        type="list-item-two-line"
+      >
+        <v-card class="mx-auto" :flat="true" max-width="344">
+          <v-card-text>
+            <p class="display-1 text--primary">
+              {{ getTotalPeoplesWithSymptoms }}
+            </p>
+            <div class="text--primary">{{ title_three }}</div>
+          </v-card-text>
+        </v-card>
+      </v-skeleton-loader>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "HighLevelStatistics",
+  components: {},
+  data() {
+    return {
+      title_one: "TOTAL SYMPTOMS REGISTERED",
+      title_two: "MOST COMMON SYMPTOM",
+      title_three: "PEOPLE WITH SYMPTOMS"
+    };
+  },
+  methods: {
+    ...mapActions(["fetchTotalSymptoms", "fetchTotalPeoplesWithSymptoms"])
+  },
+  computed: {
+    ...mapGetters([
+      "getTotalSymptoms",
+      "getMostCommonSymptom",
+      "getMostCommonSymptomCount",
+      "getTotalPeoplesWithSymptoms",
+      "getSymptomStatLoaders"
+    ])
+  },
+  created() {
+    this.fetchTotalSymptoms();
+    this.fetchTotalPeoplesWithSymptoms();
+  }
+};
+</script>
