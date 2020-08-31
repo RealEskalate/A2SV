34a2f73f70957dbd7029f7133c3230db73e24735
MohammedAlewi <rofyalewi@gmail.com>
2020-08-31 16:38:07 +0300

    [api] Adding models for new features...

---
+++ Backend/models/CaseInvestigation.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const caseInvestigationSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+
+    patient_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Patient'
+    },
+    assigned_to: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    reserved: {
+        type: Boolean,
+    },
+    notes: {
+        type: String,
+        required: true
+    },
+
+},
+{
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+});
+
+
+const CaseInvestigation = mongoose.model("CaseInvestigation", caseInvestigationSchema);
+exports.CaseInvestigation = CaseInvestigation;
