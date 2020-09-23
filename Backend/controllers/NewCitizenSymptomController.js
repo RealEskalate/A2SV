1b986a86668232f653dfd76948d85e98d778b4fd
Anteneh Admasu <39303685+AntenehAdmasu@users.noreply.github.com>
2020-09-23 17:17:06 +0300

    [api] added separate arrays for statuses (#423)
    * [api] added separate arrays for statuses
    
    * [api] test stat - dates sorted and 0 values inserted

--- Backend/controllers/NewCitizenSymptomController.js
+++ Backend/controllers/NewCitizenSymptomController.js
@@ -1,9 +1,10 @@
-const {NewCitizenSymptoms, NewCitizenSymptomsDemo } = require("../models/NewCitizenSymptomsModel");
+const { NewCitizenSymptoms, NewCitizenSymptomsDemo } = require("../models/NewCitizenSymptomsModel");
 const { SymptomUserHistory } = require("../models/SymptomUserHistoryModel");
 const { User } = require("../models/UserModel");
 const { PatientLog, PatientLogDemo } = require("../models/PatientLog.js");
-const { SymptomLog,SymptomLogDemo } = require("../models/SymptomLogModel");
+const { SymptomLog, SymptomLogDemo } = require("../models/SymptomLogModel");
 const { DistrictModel } = require("../models/DistrictModel");
+const _ = require("lodash");
 
 const mongoose = require("mongoose");
 
@@ -33,7 +34,7 @@ exports.get_new_citizens_with_symptoms = async (req, res) => {
     let startDate = new Date(Date.parse(setStartDate(req) + "T21:00:00.000Z"));
     let endDate = new Date(Date.parse(setEndDate(req) + "T21:00:00.000Z"));
 
-    let newCitizenModel = (req.query.demo)? NewCitizenSymptomsDemo :  NewCitizenSymptoms;
+    let newCitizenModel = (req.query.demo) ? NewCitizenSymptomsDemo : NewCitizenSymptoms;
 
     const new_citizens = await newCitizenModel.find({
         date: {
@@ -90,8 +91,8 @@ exports.prepop = async () => {
 };
 
 // for new confirmed, death & test administered 
-exports.ephi_test_stats= async(req,res) => {
-    
+exports.ephi_test_stats = async (req, res) => {
+
     let filter = {
         date: {
             $gte: new Date(Date.parse(setStartDate(req) + "T21:00:00.000Z")),
@@ -99,68 +100,101 @@ exports.ephi_test_stats= async(req,res) => {
         }
     }
 
-    if(req.query.status){
-        filter.test_status = req.query.status
-    }
+    let patientLogModel = (req.query.demo) ? PatientLogDemo : PatientLog;
+    let patientLogs = await patientLogModel.find(filter).sort({ "date": 1 });
+    let result = {};
 
-    let patientLogModel = (req.query.demo)? PatientLogDemo :  PatientLog;
-
-    
-    let patientLogs= await patientLogModel.find(filter).sort({ "date": 1 });
-
-    let result = {}
-
-    for(var index in patientLogs){
+    for (var index in patientLogs) {
         let log = patientLogs[index];
-        let date = log.date.toISOString().slice(0,10)
+        let date = log.date.toISOString().slice(0, 10);
 
-        if(date in result){
-            result[date]+=log.count;
-        }else{
-            result[date]=log.count;
+        // Populating 0s for the web graph to render
+        if (!(date in result)) {
+            result[date] = {
+                positive: 0,
+                negative: 0,
+                death: 0,
+                recovered: 0,
+                administered: 0
+            };
+        }
+        if (log.test_status == "Positive") {
+            result[date].positive += log.count;
+            result[date].administered += log.count;
+        } else if (log.test_status == "Negative") {
+            result[date].negative += log.count;
+            result[date].administered += log.count;
+        } else if (log.test_status == "Recovered") {
+            result[date].recovered += log.count;
+        } else if (log.test_status == "Died") {
+            result[date].death += log.count;
+            result[date].administered += log.count;
         }
     }
+    // Populating if a date is skipped - again for the sake of the web graph
+    const endDate = new Date(req.query.end_date);
+    const nextDate = new Date(req.query.start_date);
+    while (nextDate <= endDate) {
+        const dateKey = "" + nextDate.toISOString().slice(0, 10);
+        if (!(dateKey in result)) {
+            result[dateKey] = {
+                positive: 0,
+                negative: 0,
+                death: 0,
+                recovered: 0,
+                administered: 0
+            };
+        }
+        nextDate.setDate(nextDate.getDate() + 1);
+    }
 
-    return res.send(result);
+    // Sorting the dates
+    const sorted = {};
+    const keys = Object.keys(result);
+    keys.sort();
+    keys.forEach(function (item, index) {
+        sorted[item] = result[item];
+    });
+
+    return res.send(sorted);
 }
 
 
-exports.symptoms_count_in_district= async (req,res) =>{
-    let SymptomLogModel = (req.query.demo)? SymptomLogDemo : SymptomLog
+exports.symptoms_count_in_district = async (req, res) => {
+    let SymptomLogModel = (req.query.demo) ? SymptomLogDemo : SymptomLog
 
     let districts = await DistrictModel.find({}).select('name');
     let symptomLogs = await SymptomLogModel.find({}).select('current_symptoms.location.district');
 
-    let districtDict= {}
+    let districtDict = {}
 
-    districts.forEach( district => districtDict[district._id] = district.name )
+    districts.forEach(district => districtDict[district._id] = district.name)
 
     let symptomsCount = {}
 
-    symptomLogs.forEach( log =>  
-        {
-            if(districtDict[log.current_symptoms.location.district] in symptomsCount){
-                symptomsCount[ districtDict[log.current_symptoms.location.district] ] +=1
-            }else{
-                symptomsCount[ districtDict[log.current_symptoms.location.district] ] =1
-            }
+    symptomLogs.forEach(log => {
+        if (districtDict[log.current_symptoms.location.district] in symptomsCount) {
+            symptomsCount[districtDict[log.current_symptoms.location.district]] += 1
+        } else {
+            symptomsCount[districtDict[log.current_symptoms.location.district]] = 1
+        }
 
-        })
-        
+    })
+
 
     let districtCount = []
 
-    for(var name in symptomsCount){
-        if(name!="undefined"){
+    for (var name in symptomsCount) {
+        if (name != "undefined") {
             districtCount.push({
                 region: name,
-                count:symptomsCount[name]
+                count: symptomsCount[name]
             })
         }
 
     }
 
-    districtCount.sort((a,b) => a.count - b.count)
+    districtCount.sort((a, b) => a.count - b.count)
 
     res.send(districtCount)
 }
